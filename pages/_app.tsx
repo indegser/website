4b0360bf340bdd03ec4e14c1ff440dc3503cd9df
indegser <indegser@gmail.com>
2020-01-17 23:22:29 +0900

    fix page layout to 100vh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,29 @@ import { Provider as ReduxProvider } from 'react-redux'
 import GlobalStyle from "../app/atoms/GlobalStyle";
 import { store } from '../app/store/store';
 import Footer from '../app/organs/footer/Footer';
+import Nav from '../app/organs/Nav';
 
 export default ({ Component, pageProps }) => (
   <ReduxProvider store={store}>
     <GlobalStyle />
-    <Component {...pageProps}></Component>
-    <Footer />
+    <div className="page">
+      <Nav />
+      <main>
+        <Component {...pageProps}></Component>
+      </main>
+      <Footer />
+    </div>
+    <style jsx>{`
+      .page {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+
+      main {
+        flex: 1 1;
+        width: 100%;
+      }  
+    `}</style>
   </ReduxProvider>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ const Home = () => {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Nav />
     </div>
   )
 }
