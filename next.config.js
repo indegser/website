1ab00ffeb708a8e2f1f47a8b958e7af473dc1e50
indegser <indegser@gmail.com>
2020-01-17 19:13:09 +0900

    add svgr/webpack

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ require('dotenv').config()
 exports.default = {
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
