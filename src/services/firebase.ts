289e8860a73ac5658dcce66a21980d499c340f02
indegser <indegser@gmail.com>
2020-01-25 15:46:25 +0900

    comment firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,60 +1,60 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+// import 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "indegser-com.firebaseapp.com",
-  databaseURL: "https://indegser-com.firebaseio.com",
-  projectId: "indegser-com",
-  storageBucket: "indegser-com.appspot.com",
-  messagingSenderId: "638846793265",
-  appId: "1:638846793265:web:4f7fd7faed17aa94b9c710",
-  measurementId: "G-T87THQEECM"
-}
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: "indegser-com.firebaseapp.com",
+//   databaseURL: "https://indegser-com.firebaseio.com",
+//   projectId: "indegser-com",
+//   storageBucket: "indegser-com.appspot.com",
+//   messagingSenderId: "638846793265",
+//   appId: "1:638846793265:web:4f7fd7faed17aa94b9c710",
+//   measurementId: "G-T87THQEECM"
+// }
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
 
-firebase.auth().onAuthStateChanged(function(user) {
-  console.log(user, 'USER', !process.browser && 'SERVER_SIDE')
-  if (user) {
-    // User is signed in.
-  } else {
-    // No user is signed in.
-  }
-});
+// firebase.auth().onAuthStateChanged(function(user) {
+//   console.log(user, 'USER', !process.browser && 'SERVER_SIDE')
+//   if (user) {
+//     // User is signed in.
+//   } else {
+//     // No user is signed in.
+//   }
+// });
 
-if (process.browser) {
-  firebase.auth().getRedirectResult()
-    .then(result => console.log(result))
-}
+// if (process.browser) {
+//   firebase.auth().getRedirectResult()
+//     .then(result => console.log(result))
+// }
 
-export const signInWithGoogle = () => {
-  try {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    provider.setCustomParameters({ prompt: 'select_account' })
-    firebase.auth().signInWithRedirect(provider)
-  } catch (err) {
-    console.log(err)
-  }
-}
+// export const signInWithGoogle = () => {
+//   try {
+//     const provider = new firebase.auth.GoogleAuthProvider()
+//     provider.setCustomParameters({ prompt: 'select_account' })
+//     firebase.auth().signInWithRedirect(provider)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
-export const signInWithFacebook = () => {
-  try {
-    const provider = new firebase.auth.FacebookAuthProvider()
-    firebase.auth().signInWithPopup(provider)
-  } catch (err) {
-    console.log(err)
-  }
-}
+// export const signInWithFacebook = () => {
+//   try {
+//     const provider = new firebase.auth.FacebookAuthProvider()
+//     firebase.auth().signInWithPopup(provider)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
-export const signInWithGithub = () => {
-  try {
-    const provider = new firebase.auth.GithubAuthProvider()
-    firebase.auth().signInWithPopup(provider)
-  } catch (err) {
-    console.log(err)
-  }
-}
+// export const signInWithGithub = () => {
+//   try {
+//     const provider = new firebase.auth.GithubAuthProvider()
+//     firebase.auth().signInWithPopup(provider)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
