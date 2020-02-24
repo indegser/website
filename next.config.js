6494a78adc7788a7a1d0de3a505cad9bc9abb13d
indegser <indegser@gmail.com>
2020-02-25 00:41:51 +0900

    Update workflow

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 require('dotenv').config()
 const path = require('path')
 
-const { ADMIN_PASSWORD } = process.env
-
 module.exports = {
-  env: {
-    ADMIN_PASSWORD,
-    NOW_NAME: process.env.NOW_NAME,
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  },
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
