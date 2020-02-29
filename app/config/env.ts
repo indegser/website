74c72c4d8ba19031c20b02b79c622e1989056cb6
Jaekwon Han <indegser@gmail.com>
2020-02-29 17:49:25 +0900

    Fix mapping of github vars to client env

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,11 +1,28 @@
-const env = {
-  gitSha: process.env.GITHUB_SHA,
-  gitBranch: process.env.GITHUB_REF,
+const parseGitBranch = () => {
+  const ref = process.env.GITHUB_REF
+
+  if (!ref) {
+    return 'local' // local
+  }
+
+  const sliced = ref.split('/')
+  return sliced[sliced.length - 1]
 }
 
-console.log(
-  `Running on sha: ${env.gitSha || 'local'}. branch: ${env.gitBranch ||
-    'local'}`
-)
+const parseGitSha = () => {
+  const sha = process.env.GITHUB_SHA
+  if (!sha) {
+    return '*******' // local
+  }
+
+  return sha.slice(0, 7)
+}
+
+const env = {
+  gitSha: parseGitSha(),
+  gitBranch: parseGitBranch(),
+}
+
+console.log(`Running on sha: ${env.gitSha}. branch: ${env.gitBranch}`)
 
 export default env
--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const path = require('path')
 
+const { GITHUB_REF } = process.env
+
+console.log('can destruct?', GITHUB_REF)
+
 module.exports = {
   env: {
-    GITHUB_REF: process.env.GITHUB_SHA,
-    GITHUB_SHA: process.env.GITHUB_REF,
+    GITHUB_REF: process.env.GITHUB_REF,
+    GITHUB_SHA: process.env.GITHUB_SHA,
   },
   webpack: config => {
     config.module.rules.push({
