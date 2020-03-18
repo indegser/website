90856ecd1da8363d7917758b9b974469650d71de
Jaekwon Han <indegser@gmail.com>
2020-03-19 01:17:25 +0900

    Add edition info to choseh meta

--- app/utils/dateUtils.ts
+++ app/utils/dateUtils.ts
@@ -1,6 +1,8 @@
 import formatRelative from 'date-fns/formatRelative'
+import format from 'date-fns/format'
 
 export const dateFns = {
   formatRelative: (...args: Parameters<typeof formatRelative>) =>
     formatRelative(args[0], args[1]),
+  formatBasic: (date: number) => format(date, 'MMM d, yyyy'),
 }
