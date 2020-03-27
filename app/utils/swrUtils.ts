4a4a6e3c40db91396074fc009018a6a006b49a29
Jaekwon Han <indegser@gmail.com>
2020-03-27 22:49:33 +0900

    remove swrUtil

--- app/utils/swrUtils.ts
+++
@@ -1,8 +0,0 @@
-import useSWR from 'swr'
-
-export function useAxiosSWR<T>(key, fetcher) {
-  return useSWR<T>(key, async () => {
-    const { data } = await fetcher()
-    return data.data
-  })
-}
