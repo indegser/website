50d762cb961a498962b9722f15d3f89efe4c677b
Jaekwon Han <indegser@gmail.com>
2020-04-25 01:42:35 +0900

    Fix Shelf page

--- app/pages/shelf/Shelf.tsx
+++ app/pages/shelf/Shelf.tsx
@@ -1,35 +1,19 @@
 import PageContainer from 'design/atoms/container/PageContainer'
-import { ShelfGrid } from './Shelf.styled'
+import styles from './styles.module.scss'
 import BookCard from './book-card/BookCard'
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-import { defaultQueryOption } from 'apis/apolloClient'
-
-const GET_STORIES = gql`
-  query books {
-    books(first: 30, orderBy: [MODIFIED_AT_DESC]) {
-      nodes {
-        id
-        title
-        cover
-        citation
-        createdAt
-        modifiedAt
-      }
-    }
-  }
-`
+import useSWR from 'swr'
+import sejongApi from 'apis/sejongApi'
 
 const Shelf = () => {
-  const { data } = useQuery(GET_STORIES, defaultQueryOption)
-  console.log(data)
+  const { data } = useSWR('book', sejongApi.getBooks)
+
   return (
     <PageContainer>
-      <ShelfGrid>
-        {[].map((book) => (
+      <div className={styles.grid}>
+        {data?.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
-      </ShelfGrid>
+      </div>
     </PageContainer>
   )
 }
--- app/pages/shelf/book-card/BookCard.tsx
+++ app/pages/shelf/book-card/BookCard.tsx
@@ -1,51 +1,26 @@
 import { IBook } from 'types/dataTypes'
-import {
-  BookCardBox,
-  BookCardCover,
-  BookCardTitle,
-  BookCardCoverBox,
-  BookCardCoverShadow,
-  BookCardLight,
-  BookCardInfoBox,
-  BookCardCitation,
-} from './BookCard.styled'
-import { dateFns } from 'utils/dateUtils'
-import { CitationText } from 'design/atoms/typography/LabelText'
-import Box from 'design/atoms/box/Box'
-import Link from 'next/link'
+import styles from './styles.module.scss'
 
-interface IProps {
+interface Props {
   book: IBook
 }
 
-const BookCard: React.FC<IProps> = ({ book }) => {
-  const added = `Added, ${dateFns.formatRelative(book.createdAt, Date.now())}`
-
+const BookCard: React.FC<Props> = ({ book }) => {
   return (
-    <Link as={`/b/${book.id}`} href={`/b/[bookId]`}>
-      <a>
-        <BookCardBox>
-          <BookCardCoverBox>
-            <BookCardCoverShadow
-              style={{ backgroundImage: `url(${book.cover})` }}
-            />
-            <BookCardCover src={book.cover} />
-            <BookCardLight />
-          </BookCardCoverBox>
-          <BookCardInfoBox>
-            <Box mt={3}>
-              <BookCardTitle>{book.title}</BookCardTitle>
-            </Box>
-            <Box mt={2}>
-              <BookCardCitation>{book.citation}</BookCardCitation>
-            </Box>
-            <Box mt={1}>
-              <BookCardCitation>{added}</BookCardCitation>
-            </Box>
-          </BookCardInfoBox>
-        </BookCardBox>
-      </a>
-    </Link>
+    <div className={styles.box}>
+      <div className={styles.cover}>
+        <div
+          className={styles.coverShadow}
+          style={{ backgroundImage: `url(${book.cover})` }}
+        />
+        <img className={styles.coverImage} src={book.cover} />
+        <div className={styles.coverLight} />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.title}>{book.title}</div>
+        <div className={styles.citation}>{book.citation}</div>
+      </div>
+    </div>
   )
 }
 
