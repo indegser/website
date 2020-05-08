4a3ef89a5c4a1bac9c764099ea14ca1aace72154
Jaekwon Han <indegser@gmail.com>
2020-05-09 05:02:39 +0900

    Use SEJONG_URL env

--- next.config.js
+++ next.config.js
@@ -4,12 +4,14 @@ const path = require('path')
 const {
   NOW_GITHUB_COMMIT_REF: GITHUB_REF,
   NOW_GITHUB_COMMIT_SHA: GITHUB_SHA,
+  SEJONG_URL,
 } = process.env
 
 module.exports = {
   env: {
     GITHUB_REF,
     GITHUB_SHA,
+    SEJONG_URL,
   },
   webpack: (config) => {
     config.module.rules.push({
