7411b1d9531e6359d5d85faed8c9c938eaee9783
Jaekwon Han <indegser@gmail.com>
2020-05-23 12:57:44 +0900

    Restyle heading using sans-serif

--- src/common/organs/markdown/renderer/HeadingRenderer.tsx
+++ src/common/organs/markdown/renderer/HeadingRenderer.tsx
@@ -1,6 +1,7 @@
 import { createElement, useEffect, useRef, useState } from "react";
 import GithubSlugger from "github-slugger";
 import styled from "@emotion/styled";
+import { mq } from "common/theme";
 
 const Layout = styled.div`
   h1,
@@ -8,10 +9,29 @@ const Layout = styled.div`
   h3,
   h4,
   h5 {
-    font-weight: 500;
-    letter-spacing: -0.02em;
-    word-break: break-word;
+    font-weight: bold;
+    word-break: keep-all;
+    text-align: left;
+    max-width: 320px;
+    line-height: 1.3;
+    margin-top: 48px;
+    font-family: var(--font-serif);
   }
+
+  h1 {
+    margin-left: -24px;
+
+    ${mq("md")} {
+      margin-left: 0px;
+    }
+  }
+`;
+
+const HeadingDivider = styled.div`
+  width: 24px;
+  height: 4px;
+  background: var(--text400);
+  margin-bottom: 24px;
 `;
 
 const HeadingRenderer = ({ level, children }) => {
@@ -29,6 +49,7 @@ const HeadingRenderer = ({ level, children }) => {
   return (
     <Layout id={id} ref={ref}>
       {heading}
+      {level === 1 && <HeadingDivider />}
     </Layout>
   );
 };
