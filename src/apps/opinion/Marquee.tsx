fc75fb45925e10abc3aea03c546009a85f3c192c
Jaekwon Han <indegser@gmail.com>
2020-06-10 00:43:19 +0900

    Add Author to Opinion

--- src/apps/opinion/Marquee.tsx
+++ src/apps/opinion/Marquee.tsx
@@ -1,12 +1,12 @@
-import { IStory } from 'types/dataTypes'
-import { FC, useMemo } from 'react'
-import styled from '@emotion/styled'
-import Link from 'next/link'
-import { dateFns } from 'utils/dateUtils'
-import { capitalize } from 'utils/stringUtils'
+import { IStory } from "types/dataTypes";
+import { FC, useMemo } from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
+import { dateFns } from "utils/dateUtils";
+import { capitalize } from "utils/stringUtils";
 
 interface Props {
-  story: IStory
+  story: IStory;
 }
 
 const MarqueeBox = styled.div`
@@ -17,56 +17,58 @@ const MarqueeBox = styled.div`
   display: flex;
   padding-bottom: 16px;
   margin-bottom: 16px;
+  font-family: var(--font-serif);
 
   p {
     margin: 0.5rem 0;
   }
-`
+`;
 
 const MarqueeContent = styled.div`
   margin-right: 12px;
   flex: 1 1;
-`
+`;
 
 const MarqueeTitle = styled.div`
   font-size: 16px;
-  font-weight: 500;
+  font-weight: 700;
   line-height: 1.35;
   margin-bottom: 4px;
   color: var(--text400);
-`
+`;
 
 const MarqueeExcerpt = styled.div`
-  font-size: 14px;
+  font-size: 13px;
   line-height: 20px;
-  color: var(--text200);
-`
+  color: var(--text300);
+`;
 
 const MarqueeDate = styled.div`
   font-size: 13px;
   line-height: 16px;
   color: var(--text200);
   margin-top: 12px;
-`
+  font-family: var(--font-sans);
+`;
 
 const MarqueeCover = styled.img`
   flex: 0 0 auto;
   width: 120px;
   height: auto;
-`
+`;
 
 const Marquee: FC<Props> = ({ story }) => {
   const relDate = useMemo(() => {
-    const text = dateFns.formatRelative(story.modifiedAt, Date.now())
-    return capitalize(text)
-  }, [story.modifiedAt])
+    const text = dateFns.formatRelative(story.modifiedAt, Date.now());
+    return capitalize(text);
+  }, [story.modifiedAt]);
 
   const linkProps = {
-    href: '/story/[...slug]',
+    href: "/story/[...slug]",
     as: `/story/${story.slug}`,
-  }
+  };
 
-  if (!story.data.title) return null
+  if (!story.data.title) return null;
 
   return (
     <MarqueeBox>
@@ -87,7 +89,7 @@ const Marquee: FC<Props> = ({ story }) => {
         </Link>
       )}
     </MarqueeBox>
-  )
-}
+  );
+};
 
-export default Marquee
+export default Marquee;
--- src/apps/opinion/Opinion.tsx
+++ src/apps/opinion/Opinion.tsx
@@ -4,18 +4,42 @@ import PageContainer from "common/atoms/container/PageContainer";
 import sejongApi from "apis/sejongApi";
 import NewsGrid from "common/organs/grid/NewsGrid";
 import Marquee from "./Marquee";
+import Author from "./Author";
+import { mq } from "common/theme";
 
 const Container = styled.div`
-  padding: 20px 0 40px 0;
-  border-top: 1px solid var(--border400);
+  padding: 24px 0 40px 0;
   position: relative;
+  margin-top: 20px;
 
-  &:before {
-    content: "";
-    position: absolute;
-    top: 3px;
-    width: 100%;
+  ${mq("md")} {
+    margin-top: 0;
+  }
+`;
+
+const NewsContent = styled.div`
+  position: relative;
+  ${mq("md")} {
+    padding-top: 20px;
     border-top: 1px solid var(--border400);
+    &:before {
+      content: "";
+      position: absolute;
+      top: 3px;
+      width: 100%;
+      border-top: 1px solid var(--border400);
+    }
+  }
+`;
+
+const Content = styled.div`
+  display: grid;
+  grid-gap: 40px;
+  grid-template-columns: max-content auto;
+
+  ${mq("md")} {
+    grid-gap: 20px;
+    grid-template-columns: auto;
   }
 `;
 
@@ -25,11 +49,16 @@ const Opinion = () => {
   return (
     <PageContainer>
       <Container>
-        <NewsGrid>
-          {data?.map((story) => (
-            <Marquee key={story.id} story={story} />
-          ))}
-        </NewsGrid>
+        <Content>
+          <Author />
+          <NewsContent>
+            <NewsGrid>
+              {data?.map((story) => (
+                <Marquee key={story.id} story={story} />
+              ))}
+            </NewsGrid>
+          </NewsContent>
+        </Content>
       </Container>
     </PageContainer>
   );
