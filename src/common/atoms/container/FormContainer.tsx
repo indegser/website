daa3d3d2d479cf826a1a2f3e135a9cb5a0d6e405
Jaekwon Han <indegser@gmail.com>
2020-07-25 20:02:15 +0900

    Add rc-tabs and Preview page (#75)

--- src/common/atoms/container/FormContainer.tsx
+++ src/common/atoms/container/FormContainer.tsx
@@ -1,10 +1,10 @@
-import styled from '@emotion/styled'
-import PageContainer from './PageContainer'
+import styled from "@emotion/styled";
+import PageContainer from "./PageContainer";
 
 const FormContainer = styled(PageContainer)`
   padding-top: 16px;
   padding-bottom: 16px;
-  max-width: 640px;
-`
+  max-width: 840px;
+`;
 
-export default FormContainer
+export default FormContainer;
