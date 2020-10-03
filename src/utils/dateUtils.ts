e6ebfe77ec4851a6fc3c195f483bd1b2d6a1ceda
Jaekwon Han <indegser@gmail.com>
2020-10-04 01:26:46 +0900

    Restyle opinion as inline style

--- src/utils/dateUtils.ts
+++
@@ -1,7 +0,0 @@
-import formatRelative from "date-fns/formatRelative";
-import format from "date-fns/format";
-
-export const dateFns = {
-  formatRelative: (a: number, b: any) => formatRelative(a, b),
-  formatBasic: (date: number) => format(date, "MMM d, yyyy"),
-};
