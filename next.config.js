7fc78d57eb2a80d06340756b410030b0cabe9b15
Jaekwon Han <indegser@gmail.com>
2020-11-07 16:39:08 +0900

    Restore mdx and raw-loader

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.mdx?$/,
+        use: ["raw-loader"],
+      }
+    );
 
     return config;
   },
