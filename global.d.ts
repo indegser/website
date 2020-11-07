b46e07b528555a967d04f75ad9474521054d9769
Jaekwon Han <indegser@gmail.com>
2020-11-07 16:33:47 +0900

    Update nextjs to v10

--- global.d.ts
+++ global.d.ts
@@ -2,8 +2,3 @@ declare module "*.svg" {
   const content: any;
   export default content;
 }
-
-declare module "*.md" {
-  const content: string;
-  export default content;
-}
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  modulePaths: ['<rootDir>/src/', 'node_modules'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/mock/fileMock.tsx',
-  },
-}
+  modulePaths: ["<rootDir>/src/", "node_modules"],
+};
