776461d2659adaf2c88c4fc42054e4a5eab3bd72
Jaekwon Han <han.jaekwon@naverlabs.com>
2021-01-16 17:49:56 +0900

    Add NavMenu for navigating to Books page

---
+++ src/common/organs/nav/NavMenu.tsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import { COLORS } from "common/theme";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const MenuItem = styled.a`
+  font-size: 0.925rem;
+  cursor: pointer;
+
+  &[aria-current="true"] {
+    color: ${COLORS.textLightGrey};
+    pointer-events: none;
+  }
+`;
+
+const NavMenu = () => {
+  const { pathname } = useRouter();
+
+  const items = [
+    { href: "/", name: "Issues" },
+    { href: "/books", name: "Books" },
+  ];
+
+  return (
+    <>
+      {items.map((item) => (
+        <Link href={item.href} passHref>
+          <MenuItem aria-current={pathname === item.href}>{item.name}</MenuItem>
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default NavMenu;
