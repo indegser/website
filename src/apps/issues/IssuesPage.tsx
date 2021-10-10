8a1255cbe2004fb2d0af1cc0561b639e1f87e752
indegser <indegser@gmail.com>
2021-10-10 21:42:36 +0900

    Add pagination at home

--- src/apps/issues/IssuesPage.tsx
+++ src/apps/issues/IssuesPage.tsx
@@ -1,21 +1,42 @@
 import { PageContainer } from "common/atoms/Container";
 import { IssueItem } from "./IssueItem";
-import { IssueListType } from "apis/github";
 import { SEO } from "common/SEO";
+import useSWRInfinite from "swr/infinite";
+import { IssueConnection, PageInfo } from "@octokit/graphql-schema";
+import githubApi from "apis/github";
 
 interface Props {
-  issues: IssueListType;
+  issues: IssueConnection;
 }
 
 export const IssuesPage = ({ issues }: Props) => {
-  const contents = issues.map((issue) => (
-    <IssueItem key={issue.id} issue={issue} />
-  ));
+  const { totalCount } = issues;
+  const { data, size, setSize } = useSWRInfinite(
+    (_, prevData) => {
+      if (prevData && !prevData.pageInfo.hasNextPage) return null;
 
+      return [prevData?.pageInfo ?? null];
+    },
+    (pageInfo: PageInfo | null) => {
+      return githubApi.getIssues({ after: pageInfo?.endCursor });
+    },
+    {
+      fallbackData: [issues],
+    }
+  );
+
+  const contents = data
+    .flatMap((issues) => issues.nodes)
+    .map((issue) => <IssueItem key={issue.id} issue={issue} />);
+
+  const leftover = totalCount - contents.length;
   return (
     <PageContainer>
       <SEO title="Pages - Indegser" />
       {contents}
+      {leftover > 0 ? (
+        <div onClick={() => setSize(size + 1)}>{leftover}개 더 불러오기</div>
+      ) : null}
     </PageContainer>
   );
 };
