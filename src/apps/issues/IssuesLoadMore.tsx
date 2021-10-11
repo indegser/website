347467f8c1510529527605693f39d4f79e6403eb
indegser <indegser@gmail.com>
2021-10-11 16:26:18 +0900

    feat: Use infinite loading in LoadMore

--- src/apps/issues/IssuesLoadMore.tsx
+++ src/apps/issues/IssuesLoadMore.tsx
@@ -1,26 +1,53 @@
 import styled from "@emotion/styled";
-import { mediaQueries } from "common/theme";
+import { mediaQueries, mq } from "common/theme";
+import { useEffect } from "react";
 import { ComponentProps } from "react";
+import { useInView } from "react-intersection-observer";
 import { colors } from "style.types";
 
 interface Props extends ComponentProps<typeof Container> {
   leftover: number;
+  onLoadMore: () => void;
 }
 
-export const IssuesLoadMore = ({ leftover, ...props }: Props) => {
+export const IssuesLoadMore = ({ leftover, onLoadMore, ...props }: Props) => {
   const canRender = leftover > 0;
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (leftover === 0 || !inView) return;
+    onLoadMore();
+  }, [inView, leftover]);
+
   if (!canRender) return null;
 
   return (
-    <Container {...props}>
-      <Button role="button" tabIndex={0}>
-        <strong>{leftover}개 더</strong> 불러오기
-      </Button>
+    <Container>
+      <Desktop onClick={onLoadMore}>
+        <Button role="button" tabIndex={0}>
+          <strong>{leftover}개 더</strong> 불러오기
+        </Button>
+      </Desktop>
+      <Mobile ref={ref} />
     </Container>
   );
 };
 
 const Container = styled.div``;
+
+const Desktop = styled.div`
+  ${mq("sm")} {
+    display: none;
+  }
+`;
+
+const Mobile = styled.div`
+  display: none;
+
+  ${mq("sm")} {
+    display: block;
+  }
+`;
 
 const Button = styled.div`
   padding: 4px;
