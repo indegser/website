5921d2ba4ed8251c6298863b20c759dc61ae7f98
indegser <indegser@gmail.com>
2021-11-12 21:52:11 +0900

    Add hotjar script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,17 @@ const Main = styled.div``;
 Router.events.on("routeChangeComplete", Analytics.pageView);
 
 const App = ({ Component, pageProps }) => {
+  const hotjarScript = `
+  (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2698426,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  `;
+
   return (
     <>
       <GlobalStyle />
@@ -38,6 +49,7 @@ const App = ({ Component, pageProps }) => {
           rel="stylesheet"
         ></link>
         <title>Home</title>
+        <script dangerouslySetInnerHTML={{ __html: hotjarScript }} />
       </Head>
       <Page>
         <Nav />
