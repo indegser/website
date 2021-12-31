ea3c0194f4a8b71921523b3f9b06878600cef02b
indegser <indegser@gmail.com>
2021-12-31 20:25:13 +0900

    Remove logging

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -3,6 +3,4 @@ type AppEnvType = "production" | "preview" | "development";
 export const environment =
   (process.env.NEXT_PUBLIC_VERCEL_ENV as AppEnvType) || "development";
 
-console.log(environment, process.env.NEXT_PUBLIC_VERCEL_ENV, "ENVIRONMENT");
-
 export const isProduction = environment === "production";
