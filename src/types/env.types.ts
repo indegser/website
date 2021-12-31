294ee0915e3b18cef7ba3e95fd88a625671508ac
indegser <indegser@gmail.com>
2021-12-31 20:19:18 +0900

    Add env logging

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -3,6 +3,6 @@ type AppEnvType = "production" | "preview" | "development";
 export const environment =
   (process.env.NEXT_PUBLIC_VERCEL_ENV as AppEnvType) || "development";
 
-console.log(environment, "ENVIRONMENT");
+console.log(environment, process.env.NEXT_PUBLIC_VERCEL_ENV, "ENVIRONMENT");
 
 export const isProduction = environment === "production";
