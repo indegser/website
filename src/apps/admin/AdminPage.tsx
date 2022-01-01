d77629f233fe3637fd1ab84c3e16610a91525125
indegser <indegser@gmail.com>
2022-01-01 23:51:41 +0900

    Add sign out feature

--- src/apps/admin/AdminPage.tsx
+++ src/apps/admin/AdminPage.tsx
@@ -2,9 +2,12 @@ import { Input } from "reakit/Input";
 import { Button } from "reakit/Button";
 import styled from "@emotion/styled";
 import { useAdminAuth } from "./AdminPage.hooks";
+import { useIsAdmin } from "common/hooks/admin.hooks";
+import { colors } from "style.types";
 
 export const AdminPage = () => {
-  const { form, handleSignIn } = useAdminAuth();
+  const isAdmin = useIsAdmin();
+  const { form, handleSignIn, handleSignOut } = useAdminAuth();
   const {
     formState: { errors },
   } = form;
@@ -13,10 +16,23 @@ export const AdminPage = () => {
     <Container>
       <FormBox>
         <form onSubmit={handleSignIn}>
-          <Input type="email" {...form.register("email")} />
-          <Input type="password" {...form.register("password")} />
+          <StyledInput
+            type="email"
+            placeholder="E-mail"
+            {...form.register("email")}
+          />
+          <StyledInput
+            type="password"
+            placeholder="Password"
+            {...form.register("password")}
+          />
           <Button type="submit">관리자 계정 로그인</Button>
         </form>
+        {isAdmin && (
+          <Button type="button" onClick={handleSignOut}>
+            Sign out
+          </Button>
+        )}
       </FormBox>
     </Container>
   );
@@ -30,3 +46,23 @@ const Container = styled.div`
 `;
 
 const FormBox = styled.div``;
+
+const StyledInput = styled(Input)`
+  width: 240px;
+  background: ${colors.gray50};
+  border-color: ${colors.gray200};
+  color: ${colors.gray900};
+
+  &:hover,
+  &:focus {
+    background: ${colors.gray50} !important;
+  }
+
+  &:focus {
+    box-shadow: 0 0 2px 3px ${colors.blue400};
+  }
+
+  & + & {
+    margin-top: 1rem;
+  }
+`;
