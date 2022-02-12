bb587c6a683eea7aaf3e0dfc2e42b98721a2d72f
indegser <indegser@gmail.com>
2022-02-12 20:48:05 +0900

    fix no ogimage urls

--- pages/api/og.ts
+++ pages/api/og.ts
@@ -10,14 +10,16 @@ type Data = {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const url = req.query.url.toString();
+  const result = await unfurl(url);
+
   const {
     title,
     description,
     favicon,
-    open_graph: {
-      images: [{ url: imageUrl }],
-    },
-  } = await unfurl(url);
+    open_graph: { images },
+  } = result;
+
+  const imageUrl = images ? images[0].url : null;
   res.status(200).json({ title, description, favicon, imageUrl });
 };
 
