b0774bdbf3544bb53151fb38b3391e8368401101
indegser <indegser@gmail.com>
2022-02-12 21:47:56 +0900

    Add open-graph-scraper

--- pages/api/og.ts
+++ pages/api/og.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { unfurl } from "unfurl.js";
+import ogs from "open-graph-scraper";
 
 type Data = {
   title: string;
@@ -10,20 +10,32 @@ type Data = {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const url = req.query.url.toString();
-  const result = await unfurl(url, {
-    userAgent: "Googlebot",
+
+  const { result } = await ogs({
+    url,
+    headers: {
+      "user-agent":
+        "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
+    },
   });
 
-  console.log(result, "RESULT");
-  const {
-    title,
-    description,
-    favicon,
-    open_graph: { images },
-  } = result;
+  if (!result.success) {
+    res.status(400).end();
+    return;
+  }
 
-  const imageUrl = images ? images[0].url : null;
-  res.status(200).json({ title, description, favicon, imageUrl });
+  const { ogTitle, ogDescription, ogImage, favicon } = result;
+
+  let imageUrl: string = null;
+  if (Array.isArray(ogImage)) {
+    imageUrl = ogImage[0].url;
+  } else if (ogImage) {
+    imageUrl = ogImage.url;
+  }
+
+  res
+    .status(200)
+    .json({ title: ogTitle, description: ogDescription, favicon, imageUrl });
 };
 
 export default handler;
