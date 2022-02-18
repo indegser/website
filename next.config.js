2fdc26095fb7f4c87a8dfbc295980cea1e37d804
indegser <indegser@gmail.com>
2022-02-19 03:20:51 +0900

    Add sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 const moduleExports = {
   swcMinify: true,
   env: {
@@ -5,20 +7,23 @@ const moduleExports = {
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000",
   },
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
-      {
-        test: /\.mdx?$/,
-        use: ["raw-loader"],
-      }
-    );
-
-    return config;
-  },
+  // webpack: (config) => {
+  //   return config;
+  // },
 };
 
-module.exports = moduleExports;
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
