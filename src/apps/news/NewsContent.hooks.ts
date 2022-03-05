f6eb5de3a7569330706d36198711bb4a1e773c96
indegser <indegser@gmail.com>
2022-03-05 15:09:59 +0900

    feat: Use default meta

--- src/apps/news/NewsContent.hooks.ts
+++ src/apps/news/NewsContent.hooks.ts
@@ -5,65 +5,24 @@ import {
   useNewsQuery,
   useNewsQueryKey,
 } from "queries/useNewsQuery";
-import { Descendant, Element, Node } from "slate";
+import { Descendant } from "slate";
 import { mutate } from "swr";
 import debounce from "lodash-es/debounce";
 import { extraApi } from "apis/extra";
 import { captureException } from "@sentry/nextjs";
 import { useRouter } from "next/router";
-
-const extractTitle = (content: Descendant[]) => {
-  const headlineNode = content.find((node) => node.type === "headline");
-  return headlineNode ? Node.string(headlineNode) : "Untitled";
-};
-
-const extractExcerpt = (nodes: Descendant[]) => {
-  let description = "";
-  for (const node of nodes) {
-    if (node.type !== "paragraph") continue;
-
-    const text = Node.string(node).trim();
-    description += text;
-    description = description.replace(/\.?$/, `. `);
-
-    if (description.length > 300) {
-      description.trim();
-      break;
-    }
-  }
-
-  return description;
-};
-
-const extractCoverUrl = (nodes: Descendant[]) => {
-  for (const node of nodes) {
-    if (!Element.isElement(node)) {
-      continue;
-    }
-
-    if (node.type === "image") {
-      return node.url;
-    }
-
-    if (node.type === "bookmark") {
-      return node.openGraph?.imageUrl;
-    }
-  }
-
-  return;
-};
+import { useNewsMeta } from "./NewsSeo.hooks";
 
 export const useNewsContent = () => {
   const key = useNewsQueryKey();
   const router = useRouter();
   const { data: news } = useNewsQuery();
+  const { extractNewsMeta } = useNewsMeta();
 
   const autoSaveNewsContent = async (nextContent: Descendant[]) => {
     const nextNews: NewsType = {
       ...news,
-      title: extractTitle(nextContent),
-      excerpt: extractExcerpt(nextContent),
-      cover_url: extractCoverUrl(nextContent),
+      ...extractNewsMeta(nextContent),
       content: JSON.stringify(nextContent),
     };
 
