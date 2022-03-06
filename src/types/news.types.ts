9fbead445cc5ec18b6e11c4a9bfe35ac66145b4f
Jaekwon Han <indegser@gmail.com>
2022-03-06 19:18:18 +0900

    Add tag feature (#144)

--- src/types/news.types.ts
+++ src/types/news.types.ts
@@ -1,10 +1,16 @@
 export interface NewsType {
   id: string;
   title: string;
-  tags: string;
+  tag: string;
   excerpt: string | null;
   content: string | null;
   cover_url: string | null;
   created_at: string;
   published_at: string;
+}
+
+export interface TagType {
+  id: string;
+  name: string;
+  created_at: string;
 }
