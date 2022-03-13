1b0f690337e9a6d882f8ab5901d2633bad5b5f26
indegser <indegser@gmail.com>
2022-03-13 22:13:05 +0900

    Add filter

--- pages/newsroom/[newsId].tsx
+++ pages/newsroom/[newsId].tsx
@@ -2,11 +2,20 @@ import { GetStaticPaths, GetStaticProps } from "next";
 
 import { News } from "@src/pages/news/News";
 import { notion } from "@src/sdks/notion";
+import { isProduction } from "@src/types/env.types";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const news = await notion.databases.query({
     database_id: "0021f4b0494546a596716a7a5d9db452",
     page_size: 20,
+    filter: isProduction
+      ? {
+          property: "status",
+          select: {
+            equals: "Production",
+          },
+        }
+      : { property: "status", select: { is_not_empty: true } },
   });
 
   const paths = news.results.map((result) => ({
