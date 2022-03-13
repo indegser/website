75e8aaed36aaf1d883d221396395e477eff0436a
indegser <indegser@gmail.com>
2022-03-13 23:05:44 +0900

    Add image block style

--- src/pages/news/ImageBlock.tsx
+++ src/pages/news/ImageBlock.tsx
@@ -1,6 +1,7 @@
 import { Caption } from "./Caption";
 
 import { styled } from "@src/common/stitches.config";
+import { mq } from "@src/common/theme";
 import { BlockType } from "@src/types/notion.types";
 
 interface Props {
@@ -19,19 +20,36 @@ export const ImageBlock = ({ block }: Props) => {
   return (
     <Container>
       <Image src={url} alt="" />
-      <Caption caption={caption} />
+      <CaptionContainer>
+        <Caption caption={caption} />
+      </CaptionContainer>
     </Container>
   );
 };
 
 const Container = styled("div", {
-  margin: "0.25rem 0",
+  margin: "44px 0",
+
+  [mq("sm")]: {
+    marginLeft: "calc(-1 * max(22px,env(safe-area-inset-left)))",
+    marginRight: "calc(-1 * max(22px,env(safe-area-inset-right)))",
+  },
 });
 
 const Image = styled("img", {
   width: "100%",
   height: "auto",
   display: "block",
-  margin: "1rem 0",
   borderRadius: "8px",
+
+  [mq("sm")]: {
+    borderRadius: 0,
+  },
+});
+
+const CaptionContainer = styled("div", {
+  [mq("sm")]: {
+    marginLeft: "max(22px,env(safe-area-inset-left))",
+    marginRight: "max(22px,env(safe-area-inset-right))",
+  },
 });
