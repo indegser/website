dee4c03d81650afe7731b2d626bcbe272a5060f5
indegser <indegser@gmail.com>
2022-03-13 22:13:05 +0900

    Add filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import { GetStaticProps } from "next";
 
 import { Newsroom } from "@src/pages/newsroom/Newsroom";
 import { notion } from "@src/sdks/notion";
+import { isProduction } from "@src/types/env.types";
 
 export const getStaticProps: GetStaticProps = async () => {
   try {
     const database = await notion.databases.query({
       database_id: "0021f4b0494546a596716a7a5d9db452",
       sorts: [{ property: "published_time", direction: "descending" }],
+      filter: isProduction
+        ? {
+            property: "status",
+            select: {
+              equals: "Production",
+            },
+          }
+        : { property: "status", select: { is_not_empty: true } },
     });
 
     return {
