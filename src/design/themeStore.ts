7813bb2f12fab003bdb1471342d97e8b4a9b0686
indegser <indegser@gmail.com>
2022-03-20 22:27:18 +0900

    Add theme cache

--- src/design/themeStore.ts
+++ src/design/themeStore.ts
@@ -2,15 +2,24 @@ import create from "zustand";
 
 type State = {
   theme: "system" | "dark" | "light";
+  restoreCachedTheme: () => void;
   setTheme: (theme: State["theme"]) => void;
-  toggleTheme: () => void;
 };
 
-export const useThemeStore = create<State>((set, get) => ({
+export type ThemeType = State["theme"];
+
+const THEME_CACHE_KEY = "indegser-theme";
+
+export const useThemeStore = create<State>((set) => ({
   theme: "light",
-  setTheme: (theme) => set({ theme }),
-  toggleTheme: () => {
-    const { theme } = get();
-    set({ theme: theme === "dark" ? "light" : "dark" });
+  restoreCachedTheme: () => {
+    const theme = localStorage.getItem(THEME_CACHE_KEY) as ThemeType;
+    if (!theme) return;
+
+    set({ theme });
+  },
+  setTheme: (theme) => {
+    set({ theme });
+    localStorage.setItem(THEME_CACHE_KEY, theme);
   },
 }));
