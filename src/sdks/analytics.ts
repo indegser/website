f7034002ad1a0c9b75cd6fec4d55743c817469ed
indegser <indegser@gmail.com>
2022-03-23 00:55:34 +0900

    Add sns and theme click event

--- src/sdks/analytics.ts
+++ src/sdks/analytics.ts
@@ -1,9 +1,7 @@
-// @ts-ignore;
-const gtag = process.browser && window.gtag;
+import { isServer } from "@src/types/env.types";
 
-/**
- * NEXT_PUBLIC_GA_TRACKING_ID는 production Vercel 환경에만 주입 됨
- */
+const gtag = !isServer && window.gtag;
+
 const trackingId = process.env.NEXT_PUBLIC_GA_TRACKING_ID ?? "";
 export namespace Analytics {
   export const id = trackingId;
@@ -13,24 +11,13 @@ export namespace Analytics {
       console.info(`Google Analytics disabled. Tracking id is empty`);
       return;
     }
+
     gtag("config", trackingId, {
       page_path: url,
     });
   }
 
-  export function reportWebVitals({ id, name, label, value }) {
-    if (!id) {
-      console.info(`Google Analytics disabled. Tracking id is empty`);
-      return;
-    }
-
-    gtag("send", "event", {
-      eventCategory:
-        label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-      eventAction: name,
-      eventValue: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-      eventLabel: id, // id unique to current page load
-      nonInteraction: true, // avoids affecting bounce rate.
-    });
+  export function event(action: string, params: Gtag.EventParams) {
+    gtag("event", action, params);
   }
 }
