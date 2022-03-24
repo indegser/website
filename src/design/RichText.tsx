26b89bc8c59514a706dfed70ba1b2a759134f807
indegser <indegser@gmail.com>
2022-03-24 20:58:54 +0900

    Render emoji in rich text

--- src/design/RichText.tsx
+++ src/design/RichText.tsx
@@ -1,3 +1,6 @@
+import { Fragment, ReactElement, ReactNode } from "react";
+import { toArray } from "react-emoji-render";
+
 import { convertApiColorToStyleProps } from "./convertApiColorToStyleProps";
 import { RichTextWithLink } from "./RichTextWithLink";
 
@@ -7,6 +10,12 @@ import { RichTextItemResponse } from "@src/types/notion.types";
 interface Props {
   shouldRenderPlainText?: boolean;
   data: RichTextItemResponse;
+}
+
+function isReactElement(
+  child: ReactElement | ReactNode
+): child is ReactElement {
+  return typeof child === "object";
 }
 
 export const RichText = ({ data, shouldRenderPlainText = false }: Props) => {
@@ -21,6 +30,9 @@ export const RichText = ({ data, shouldRenderPlainText = false }: Props) => {
               : ({} as typeof richText["annotations"]);
 
             const { link, content } = richText.text;
+            const children = toArray(content) as Array<
+              ReactNode | ReactElement
+            >;
 
             return (
               <RichTextWithLink key={i} link={link}>
@@ -29,7 +41,17 @@ export const RichText = ({ data, shouldRenderPlainText = false }: Props) => {
                   {...annotations}
                   style={convertApiColorToStyleProps(color)}
                 >
-                  {content}
+                  {children.map((child, index) => {
+                    if (isReactElement(child)) {
+                      return (
+                        <EmojiIcon key={index}>
+                          {child.props.children}
+                        </EmojiIcon>
+                      );
+                    }
+
+                    return <Fragment key={index}>{child}</Fragment>;
+                  })}
                 </Text>
               </RichTextWithLink>
             );
@@ -79,4 +101,12 @@ const Text = styled("span", {
       },
     },
   },
+});
+
+const EmojiIcon = styled("span", {
+  fontFamily:
+    '"Apple Color Emoji","Segoe UI Emoji",NotoColorEmoji,"Noto Color Emoji","Segoe UI Symbol","Android Emoji",EmojiSymbols',
+  lineHeight: 1,
+  whiteSpace: "nowrap",
+  fontSize: "1em",
 });
