7a3001228f71e0bccc045d1c2d62ccdeddb44f68
indegser <indegser@gmail.com>
2022-04-01 01:14:36 +0900

    Add notion integration

--- src/types/const.types.ts
+++ src/types/const.types.ts
@@ -5,7 +5,10 @@ export const STORY_DEFAULT_PAGE_SIZE = 20;
 export const ORIGIN =
   environment === "production"
     ? "https://www.indegser.pro"
-    : `https://${process.env.VERCEL_URL}` ?? "";
+    : `https://${process.env.VERCEL_URL}` ?? "http://localhost:3000";
 
 export const GIT_COMMIT_SHA =
   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7) ?? "";
+
+export const NOTION_CLIENT_ID = "174fb0a5-b38c-46fc-aaed-d9a94d86902e";
+export const NOTION_REDIRECT_URI = `${ORIGIN}/api/notion/callback`;
