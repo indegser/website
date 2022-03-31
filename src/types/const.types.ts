2fa82c6bf6ac4b7a5f5a8c8339983ad6feee7938
indegser <indegser@gmail.com>
2022-04-01 01:18:39 +0900

    Add strict origin

--- src/types/const.types.ts
+++ src/types/const.types.ts
@@ -5,7 +5,9 @@ export const STORY_DEFAULT_PAGE_SIZE = 20;
 export const ORIGIN =
   environment === "production"
     ? "https://www.indegser.pro"
-    : `https://${process.env.VERCEL_URL}` ?? "http://localhost:3000";
+    : environment === "preview"
+    ? "https://edge.indegser.pro"
+    : "http://localhost:3000";
 
 export const GIT_COMMIT_SHA =
   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7) ?? "";
