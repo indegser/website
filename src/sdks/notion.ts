68b17abf9f5900408b56a4fe6eb177e4067cc32d
indegser <indegser@gmail.com>
2022-04-02 00:52:34 +0900

    Add supabase sdk

--- src/sdks/notion.ts
+++ src/sdks/notion.ts
@@ -1,3 +1,6 @@
 import { Client } from "@notionhq/client";
 
 export const notion = new Client({ auth: process.env.NOTION_KEY });
+
+export const createNotionClient = (accessToken: string) =>
+  new Client({ auth: accessToken });
