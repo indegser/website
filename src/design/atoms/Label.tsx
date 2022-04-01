c84a9aff51ce58ea63a9277446eb788c1a4b1f08
indegser <indegser@gmail.com>
2022-04-02 01:57:18 +0900

    Add integrate form design

---
+++ src/design/atoms/Label.tsx
@@ -0,0 +1,8 @@
+import { styled, theme } from "@src/common/stitches.config";
+
+export const Label = styled("label", {
+  fontSize: 15,
+  color: theme.colors.blue11,
+  width: 90,
+  textAlign: "right",
+});
