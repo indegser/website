c6ea2ffbdb9b6f6a29b378cbebcb48b35dd9d2ef
indegser <indegser@gmail.com>
2022-04-02 01:34:27 +0900

    Remove custom notion callback

--- src/types/const.types.ts
+++ src/types/const.types.ts
@@ -11,6 +11,3 @@ export const ORIGIN =
 
 export const GIT_COMMIT_SHA =
   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7) ?? "";
-
-export const NOTION_CLIENT_ID = "174fb0a5-b38c-46fc-aaed-d9a94d86902e";
-export const NOTION_REDIRECT_URI = `${ORIGIN}/api/notion/callback`;
