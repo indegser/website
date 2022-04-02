ee105f9064f4f789c46b7453b5aed5eeea1427e8
indegser <indegser@gmail.com>
2022-04-02 11:09:46 +0900

    Change home to construction page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,9 @@
-import { GetStaticProps } from "next";
+import { PageContainer } from "@src/common/atoms/Container";
 
-import { Newsroom } from "@src/pages/newsroom/Newsroom";
-import { notion } from "@src/sdks/notion";
-import { isProduction } from "@src/types/env.types";
-
-const DATABASE_ID = "0021f4b0494546a596716a7a5d9db452";
-
-export const getStaticProps: GetStaticProps = async () => {
-  try {
-    const database = await notion.databases.query({
-      database_id: DATABASE_ID,
-      sorts: [{ timestamp: "last_edited_time", direction: "descending" }],
-      filter: isProduction
-        ? {
-            property: "status",
-            select: {
-              equals: "Production",
-            },
-          }
-        : { property: "status", select: { is_not_empty: true } },
-    });
-
-    return {
-      props: {
-        database,
-      },
-      revalidate: 60,
-    }; // 1min.
-  } catch (err) {
-    return { props: {} };
-  }
-};
-
-export default Newsroom;
+export default function Page() {
+  return (
+    <PageContainer>
+      <h1>Construction...</h1>
+    </PageContainer>
+  );
+}
