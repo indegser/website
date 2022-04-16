e0bab57b75f913e02d55f093b2d20785f64f8d7f
indegser <indegser@gmail.com>
2022-04-16 23:00:39 +0900

    Add type check to next config js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
+// @ts-check
+
 const { withSentryConfig } = require("@sentry/nextjs");
 
-const moduleExports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   swcMinify: true,
-  env: {
-    VERCEL_URL: process.env.VERCEL_URL,
-  },
 };
 
+/**
+ * @type {Partial<import('@sentry/nextjs').SentryWebpackPluginOptions>}
+ * https://github.com/getsentry/sentry-webpack-plugin#options.
+ */
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true,
   dryRun: process.env.VERCEL_ENV !== "production",
 };
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+// @ts-ignore
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
