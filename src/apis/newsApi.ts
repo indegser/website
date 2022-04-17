a7d7a57760aff52a350939f90a2c1d48a9c00bad
indegser <indegser@gmail.com>
2022-04-17 15:07:12 +0900

    Create newsApi

--- src/apis/newsApi.ts
+++ src/apis/newsApi.ts
@@ -1,8 +1,12 @@
 import { notion } from "@src/sdks/notion";
 
-export const getNewsDatabase = () => {
+const getNewsDatabase = () => {
   return notion.databases.query({
     database_id: "0021f4b0494546a596716a7a5d9db452",
     sorts: [{ timestamp: "last_edited_time", direction: "descending" }],
   });
+};
+
+export const newsApi = {
+  getNewsDatabase,
 };
