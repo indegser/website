b1b27bfaec3de0f3968919f0bcf17666fae5b69f
indegser <indegser@gmail.com>
2022-04-24 11:20:33 +0900

    Add retrieve newsroom api

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,11 @@ import { Newsroom } from "@src/pages/newsroom/Newsroom";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const database = await newsApi.getNewsDatabase();
+  const newsroom = await newsApi.retrieveNewsroom();
 
   return {
     props: {
+      newsroom,
       database,
     },
   };
