e8473b91d4ecacda2f9245b168a215b22501e38c
indegser <indegser@gmail.com>
2022-08-09 00:47:55 +0900

    refactor: Use NotionContent component in News

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -3,9 +3,7 @@ import { NewsHeadline } from "./NewsHeadline";
 
 import { PageContainer } from "@src/design/atoms/Container";
 import { SEO } from "@src/design/atoms/SEO";
-import { Block } from "@src/design/notion/blocks/Block";
-import { mq } from "@src/design/theme/mediaQueries";
-import { styled, theme } from "@src/design/theme/stitches.config";
+import { NotionContent } from "@src/design/notion/NotionContent";
 import { usePageTracking } from "@src/hooks/usePageTracking";
 import {
   BlockChildrenType,
@@ -28,31 +26,7 @@ export const News = ({ page, blocks }: Props) => {
     <PageContainer>
       <SEO title={title} description={excerpt} image={imageUrl} />
       <NewsHeadline news={page} />
-      <Article>
-        {blocks.map((block, index) => {
-          if (!("type" in block)) {
-            return null;
-          }
-
-          return (
-            <Block key={block.id} block={block} index={index} blocks={blocks} />
-          );
-        })}
-      </Article>
+      <NotionContent blocks={blocks} />
     </PageContainer>
   );
 };
-
-const Article = styled("article", {
-  fontSize: 16,
-  lineHeight: "26px",
-  letterSpacing: "-0.018em",
-  paddingBottom: 80,
-  fontWeight: 420,
-  color: theme.colors.gray12,
-
-  [mq("sm")]: {
-    fontSize: 18,
-    padding: "0 0 80px 0",
-  },
-});
