145bd3b6cb370888d38816d18091a1305286d6c8
indegser <indegser@gmail.com>
2022-08-09 00:41:53 +0900

    feat: Add book href

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -1,4 +1,9 @@
-import { FilesPropertyType, TitlePropertyType } from "@src/types/notion.types";
+import { notion } from "@src/sdks/notion";
+import {
+  BlockType,
+  FilesPropertyType,
+  TitlePropertyType,
+} from "@src/types/notion.types";
 
 export const getNotionFileUrl = (filesProperty: FilesPropertyType) => {
   for (const file of filesProperty.files) {
@@ -12,4 +17,39 @@ export const getNotionFileUrl = (filesProperty: FilesPropertyType) => {
 
 export const getNotionTitle = (titleProperty: TitlePropertyType) => {
   return titleProperty.title.map((text) => text.plain_text).join("");
+};
+
+export const getNotionContent = async (blockId: string) => {
+  const getBlocksWithChildren = async (blocks: BlockType[]) => {
+    return await Promise.all(
+      blocks.map(async (block) => {
+        return await getBlockWithChildren(block);
+      })
+    );
+  };
+  const getBlockWithChildren = async (block: BlockType) => {
+    if (block.has_children === true) {
+      const result = await notion.blocks.children.list({
+        block_id: block.id,
+        page_size: 100,
+      });
+
+      /**
+       * @todo has_more 체크
+       */
+
+      block["children"] = await getBlocksWithChildren(
+        result.results as BlockType[]
+      );
+    }
+
+    return block;
+  };
+
+  const children = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 100,
+  });
+
+  return getBlocksWithChildren(children.results as BlockType[]);
 };
