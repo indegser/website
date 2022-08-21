39b61157d80f5a88f8e965063001869cde3800bc
indegser <indegser@gmail.com>
2022-08-21 22:40:05 +0900

    feat: Add supabase upload in newsApi

---
+++ src/utils/image/uploadImageToSupabase.ts
@@ -0,0 +1,38 @@
+import mime from "mime-types";
+import { nanoid } from "nanoid";
+
+import { supabase } from "@src/sdks/supabase";
+
+export const uploadImageToSupabase = async (
+  imageUrl: string,
+  folder: string
+) => {
+  const response = await fetch(imageUrl);
+  const contentType = response.headers.get("Content-Type");
+  const extension = mime.extension(contentType);
+
+  if (!extension) {
+    return false;
+  }
+
+  const id = nanoid();
+  const path = `${folder}/${id}.${extension}`;
+
+  await supabase.storage.from("image").upload(path, response.body, {
+    contentType,
+  });
+
+  const { publicURL, error } = supabase.storage
+    .from("image")
+    .getPublicUrl(path);
+
+  if (error) return false;
+
+  return {
+    id,
+    path,
+    extension,
+    contentType,
+    publicURL,
+  };
+};
