4240ccc580f6ab7416ac31a76f509678a8decc38
indegser <indegser@gmail.com>
2022-08-21 14:58:26 +0900

    feat: Add seriesApi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,29 +5,32 @@ import { Footer } from "@src/design/organs/footer/Footer";
 import { Nav } from "@src/design/organs/nav/Nav";
 import { globalStyles } from "@src/design/theme/globalStyles";
 import { styled } from "@src/design/theme/stitches.config";
+import { StoreProvider } from "@src/hooks/store/StoreProvider";
 import { BaseApp } from "@src/pages/BaseApp";
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
 
   return (
-    <BaseApp>
-      <Head>
-        <title>Home</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Page>
-        <Nav />
-        <Main>
-          <Component {...pageProps} />
-        </Main>
-        <Footer />
-      </Page>
-    </BaseApp>
+    <StoreProvider value={pageProps.store}>
+      <BaseApp>
+        <Head>
+          <title>Home</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, viewport-fit=cover"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Page>
+          <Nav />
+          <Main>
+            <Component {...pageProps} />
+          </Main>
+          <Footer />
+        </Page>
+      </BaseApp>
+    </StoreProvider>
   );
 }
 
