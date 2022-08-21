590fc34080ca7bddf725b9a55d261818af552ff7
indegser <indegser@gmail.com>
2022-08-21 14:58:26 +0900

    feat: Add seriesApi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import { GetStaticProps } from "next";
 
 import { newsApi } from "@src/apis/newsApi";
+import { seriesApi } from "@src/apis/seriesApi";
 import { Newsroom } from "@src/pages/newsroom/Newsroom";
 
 export const getStaticProps: GetStaticProps = async () => {
   const database = await newsApi.getNewsDatabase();
+  const series = await seriesApi.getSeriesList();
 
   return {
     props: {
       database,
+      store: {
+        series,
+      },
     },
     revalidate: 10,
   };
