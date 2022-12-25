1c54c835d27ee4df49ffafb650ac9a09ac53c469
indegser <indegser@gmail.com>
2022-12-25 14:46:25 +0900

    feat: Add JournalList query

--- src/pages/newsroom/Newsroom.hooks.ts
+++ src/pages/newsroom/Newsroom.hooks.ts
@@ -1,33 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/router";
-import { stringifyUrl } from "query-string";
 
 import { sanity } from "@src/sdks/sanity";
-import { BookType } from "@src/types/cms.types";
-import { NewsDatabaseType } from "@src/types/news.types";
+import { BookType } from "@src/types/cms";
 
-export const useNewsroomQuery = () => {
-  const { query, isReady } = useRouter();
-  const series = query.series?.toString();
-
-  return useQuery(
-    ["newsroom", series],
-    () =>
-      fetch(stringifyUrl({ url: `/api/newsroom`, query: { series } }))
-        .then((res) => res.json())
-        .then((data) => data as NewsDatabaseType),
-    { enabled: isReady }
-  );
-};
-
-export const useBooksQuery = () => {
-  return useQuery(["book"], () => {
+export const useBookListQuery = () => {
+  return useQuery(["bookList"], () => {
     return sanity.fetch<Array<BookType>>(`
       *[_type == 'book'] {
         _id,
         title,
         "posterUrl": poster.asset->url
       }    
+    `);
+  });
+};
+
+export const useJournalListQuery = () => {
+  return useQuery(["journalList"], () => {
+    return sanity.fetch(`
+      *[_type == 'journal'] {
+        _id,
+        title,
+        content
+      }
     `);
   });
 };
