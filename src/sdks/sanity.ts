8cf04fe135ebea03bf6228d7d5968911171b30db
indegser <indegser@gmail.com>
2022-12-27 00:52:50 +0900

    feat: Use development sanity on dev

--- src/sdks/sanity.ts
+++ src/sdks/sanity.ts
@@ -1,8 +1,10 @@
 import sanityClient from "@sanity/client";
 
+import { environment } from "@src/types/env";
+
 export const sanity = sanityClient({
   projectId: "em8nd69q",
-  dataset: "production",
+  dataset: environment === "production" ? "production" : "development",
   apiVersion: "2021-03-25", // use current UTC date - see "specifying API version"!
   token: process.env.NEXT_PULBIC_SANITY_TOKEN, // or leave blank for unauthenticated usage
   useCdn: true, // `false` if you want to ensure fresh data
