7d92bdb94890d37a0618503d990e9152efac1047
indegser <indegser@gmail.com>
2022-12-27 22:44:00 +0900

    feat: Update notion version

--- src/sdks/notion.ts
+++ src/sdks/notion.ts
@@ -1,3 +1,5 @@
 import { Client } from "@notionhq/client";
 
-export const notion = new Client({ auth: process.env.NOTION_KEY });
+export const notion = new Client({
+  auth: process.env.NOTION_KEY,
+});
