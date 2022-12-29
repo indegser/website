31c90879533965bf0a9314875e1af071f09aa887
indegser <indegser@gmail.com>
2022-12-30 00:00:35 +0900

    feat: Remove sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 // @ts-check
 
-const { withSentryConfig } = require("@sentry/nextjs");
-
 /**
  * @type {import('next').NextConfig}
  */
@@ -15,14 +13,4 @@ const nextConfig = {
   },
 };
 
-/**
- * @type {Partial<import('@sentry/nextjs').SentryWebpackPluginOptions>}
- * https://github.com/getsentry/sentry-webpack-plugin#options.
- */
-const sentryWebpackPluginOptions = {
-  silent: true,
-  dryRun: process.env.VERCEL_ENV !== "production",
-};
-
-// @ts-ignore
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = nextConfig;
