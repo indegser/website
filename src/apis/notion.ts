9e7891a69758bd2e4fe807e8965b97cd8dc3d4c5
indegser <indegser@gmail.com>
2022-12-30 00:44:34 +0900

    feat: Add generic to notion api

--- src/apis/notion.ts
+++ src/apis/notion.ts
@@ -1,22 +1,26 @@
 import {
+  BlockObjectResponse,
   ListBlockChildrenParameters,
   ListBlockChildrenResponse,
+  PageObjectResponse,
   QueryDatabaseParameters,
   QueryDatabaseResponse,
 } from "@notionhq/client/build/src/api-endpoints";
 
-const getDatabase = async (
+const getDatabase = async <T = PageObjectResponse>(
   args: QueryDatabaseParameters
-): Promise<QueryDatabaseResponse> => {
+): Promise<Omit<QueryDatabaseResponse, "results"> & { results: Array<T> }> => {
   return fetch("/api/notion/database", {
     method: "POST",
     body: JSON.stringify({ args }),
   }).then((res) => res.json());
 };
 
-const getBlockChildren = async (
+const getBlockChildren = async <T = BlockObjectResponse>(
   args: ListBlockChildrenParameters
-): Promise<ListBlockChildrenResponse> => {
+): Promise<
+  Omit<ListBlockChildrenResponse, "results"> & { results: Array<T> }
+> => {
   return fetch("/api/notion/block", {
     method: "POST",
     body: JSON.stringify({ args }),
