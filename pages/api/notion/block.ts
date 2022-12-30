384bca814beed132d61427dda44aba94f24f1cc6
indegser <indegser@gmail.com>
2022-12-30 17:13:14 +0900

    feat: Add block caching with redis

--- pages/api/notion/block.ts
+++ pages/api/notion/block.ts
@@ -5,9 +5,11 @@ import {
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { notion } from "@src/sdks/notion";
+import { redis } from "@src/sdks/redis";
 
 const fetchNextPage = async (args: ListBlockChildrenParameters) => {
   const result = await notion.blocks.children.list(args);
+
   if (result.has_more) {
     const nextResult = await fetchNextPage({
       ...args,
@@ -19,12 +21,30 @@ const fetchNextPage = async (args: ListBlockChildrenParameters) => {
   return result;
 };
 
+const getRedisKey = (id: string) => {
+  return `page:${id}`;
+};
+
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ListBlockChildrenResponse>
 ) => {
-  const { args } = JSON.parse(req.body);
+  const { args }: { args: ListBlockChildrenParameters } = JSON.parse(req.body);
+  const redisKey = getRedisKey(args.block_id);
+
+  const cachedVersion = await redis.get<ListBlockChildrenResponse>(redisKey);
+
+  if (cachedVersion) {
+    res.json(cachedVersion);
+    return;
+  }
+
   const result = await fetchNextPage(args);
+
+  /**
+   * @description 1분 동안 캐싱.
+   */
+  redis.setex(redisKey, 60, JSON.stringify(result));
   res.json(result);
 };
 
