dd9804977875951ff12274616c47134dab8cbd44
indegser <indegser@gmail.com>
2022-12-30 15:52:28 +0900

    feat: Add recursive block fetch

--- pages/api/notion/block.ts
+++ pages/api/notion/block.ts
@@ -1,14 +1,32 @@
-import { ListBlockChildrenResponse } from "@notionhq/client/build/src/api-endpoints";
+import {
+  ListBlockChildrenParameters,
+  ListBlockChildrenResponse,
+} from "@notionhq/client/build/src/api-endpoints";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { notion } from "@src/sdks/notion";
+
+const fetchNextPage = async (args: ListBlockChildrenParameters) => {
+  const result = await notion.blocks.children.list(args);
+  if (result.has_more) {
+    const nextResult = await fetchNextPage({
+      ...args,
+      start_cursor: result.next_cursor,
+    });
+    result.results = [...result.results, ...nextResult.results];
+  }
+
+  return result;
+};
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ListBlockChildrenResponse>
 ) => {
   const { args } = JSON.parse(req.body);
-  const result = await notion.blocks.children.list(args);
+  console.log(args, req.method);
+
+  const result = await fetchNextPage(args);
   res.json(result);
 };
 
