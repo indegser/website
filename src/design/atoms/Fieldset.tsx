d144bf2e504ec36b754b3502bcf83e72fca67ebd
indegser <indegser@gmail.com>
2022-12-30 14:59:46 +0900

    refactor: Remove unused components

--- src/design/atoms/Fieldset.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "@src/design/theme/stitches.config";
-
-export const Fieldset = styled("fieldset", {
-  all: "unset",
-  display: "flex",
-  gap: 20,
-  alignItems: "center",
-  marginBottom: 15,
-});
