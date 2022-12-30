c3221354e2568ccd5348ca40905e787e86df2b68
indegser <indegser@gmail.com>
2022-12-31 01:33:55 +0900

    feat: Add hashtags filter

--- src/pages/newsroom/Newsroom.hooks.ts
+++ src/pages/newsroom/Newsroom.hooks.ts
@@ -1,22 +1,37 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
 import { notionApi } from "@src/apis/notion";
 import { environment } from "@src/types/env";
 import { JournalPageType } from "@src/types/notion";
 
 export const useJournalQuery = () => {
-  return useInfiniteQuery(
-    ["journal"],
-    ({ pageParam }: { pageParam?: string }) => {
+  const { query, isReady } = useRouter();
+  const hashtag = query.hashtag?.toString();
+
+  return useInfiniteQuery({
+    queryKey: ["journalList", hashtag],
+    queryFn: ({ pageParam }: { pageParam?: string }) => {
       return notionApi.getDatabase<JournalPageType>({
         database_id: "82649fda5ba84801a464d7ef2f7552b3",
         page_size: 5,
         start_cursor: pageParam,
         filter: {
-          property: "_status",
-          select: {
-            equals: environment === "production" ? "Production" : "Development",
-          },
+          and: [
+            {
+              property: "_status",
+              select: {
+                equals:
+                  environment === "production" ? "Production" : "Development",
+              },
+            },
+            hashtag && {
+              property: "Hashtags",
+              multi_select: {
+                contains: hashtag,
+              },
+            },
+          ].filter(Boolean),
         },
         sorts: [
           {
@@ -26,9 +41,8 @@ export const useJournalQuery = () => {
         ],
       });
     },
-    {
-      refetchOnWindowFocus: false,
-      getNextPageParam: (lastPage) => lastPage.next_cursor,
-    }
-  );
+    enabled: isReady,
+    refetchOnWindowFocus: false,
+    getNextPageParam: (lastPage) => lastPage.next_cursor || undefined,
+  });
 };
