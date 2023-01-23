f22bf331b087080d7d7e4c33085702a03bc9af9d
indegser <indegser@gmail.com>
2023-01-23 20:01:53 +0900

    Update newsroom to use typed supabase

--- src/queries/useJournalQueries.ts
+++ src/queries/useJournalQueries.ts
@@ -1,16 +1,17 @@
 import { useQueries } from "@tanstack/react-query";
 
 import { supabase } from "@src/sdks/supabase";
+import { BlockType } from "@src/types/notion";
 
 export const useJournalQueries = (journalIds: string[] = []) => {
   return useQueries({
     queries: journalIds.map((id) => {
       return {
         queryKey: ["journal", id],
-        queryFn: () =>
+        queryFn: async () =>
           supabase
             .from("pages")
-            .select("data")
+            .select<"data", { data: { results: BlockType[] } }>("data")
             .eq("id", id)
             .single()
             .then((result) => result.data.data),
