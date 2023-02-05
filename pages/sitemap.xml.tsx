1e6a431862953bb5c30c2c0f5eac600fd2a917f5
indegser <indegser@gmail.com>
2023-02-06 00:40:50 +0900

    Use journal data to generate sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,24 +1,38 @@
 import { GetServerSideProps } from "next";
 
-const content = `
+import { journalApi } from "@src/apis/journal";
+import { ORIGIN } from "@src/types/constants";
+import { JournalPageType } from "@src/types/notion";
+
+const generateSitemap = (journals: JournalPageType[]) => `
 <?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://www.indegser.com/</loc>
+    <loc>https://www.indegser.com</loc>
   </url>
+  ${journals
+    .map(({ id }) => {
+      return `
+    <url>
+        <loc>${`${ORIGIN}/journal/${id}`}</loc>
+    </url>
+  `;
+    })
+    .join('')}
 </urlset>
 `.trim();
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const { data } = await journalApi.fetchJournalList({ pageSize: 100 });
+
   res.setHeader("Content-Type", "text/xml");
-  res.write(content);
+  res.write(generateSitemap(data));
   res.end();
+
 
   return { props: {} };
 };
 
-function SitemapPage() {
-  return null;
-}
+function SitemapPage() {}
 
 export default SitemapPage;
