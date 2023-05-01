2b5da592cf4379e6fe41fbb635f4ebfd4128b046
indegser <indegser@gmail.com>
2023-05-01 14:15:11 +0900

    feat: Add balancer to headline

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      "cdn.sanity.io",
-      process.env.NEXT_PUBLIC_SUPABASE_URL?.replace("https://", "") || "",
+      'cdn.sanity.io',
+      process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '') || '',
     ],
   },
 };
