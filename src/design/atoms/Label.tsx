7b2ab231419cdcd52256613a8209fbfaa2a6806b
indegser <indegser@gmail.com>
2023-05-06 02:22:26 +0900

    feat: Add emotion to nav

--- src/design/atoms/Label.tsx
+++
@@ -1,8 +0,0 @@
-import { styled, theme } from '@src/design/theme/stitches.config';
-
-export const Label = styled('label', {
-  fontSize: 15,
-  color: theme.colors.blue11,
-  width: 90,
-  textAlign: 'right',
-});
--- src/design/organs/nav/Nav.tsx
+++ src/design/organs/nav/Nav.tsx
@@ -1,15 +1,16 @@
+import styled from '@emotion/styled';
 import Link from 'next/link';
 
 import { Logo } from './Logo';
 
 import { PageContainer } from '@src/design/atoms/Container';
-import { styled, theme } from '@src/design/theme/stitches.config';
+import { theme } from '@src/design/theme/stitches.config';
 
 export const Nav = () => {
   return (
     <Backdrop>
       <PageContainer>
-        <Layout id="global-nav">
+        <Layout>
           <Link href="/" passHref>
             <Heading>
               <Logo />
@@ -22,28 +23,28 @@ export const Nav = () => {
   );
 };
 
-const Backdrop = styled('div', {
-  position: 'sticky',
-  top: 0,
-  backdropFilter: `blur(12px)`,
-  backgroundColor: theme.colors.backdrop,
-  zIndex: 1,
-});
+const Backdrop = styled.div`
+  position: sticky;
+  top: 0;
+  backdrop-filter: blur(12px);
+  z-index: 1;
+  background-color: ${theme.colors.backdrop.computedValue};
+`;
 
-const Layout = styled('nav', {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: `4px 0px 2px 0px`,
-});
+const Layout = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4px 0px 2px 0px;
+`;
 
-const Heading = styled('div', {
-  display: 'flex',
-  alignItems: 'center',
-  padding: '0 16px 0 0',
-  height: 45,
-  fontSize: 14,
-  fontWeight: 600,
-  cursor: 'pointer',
-  color: theme.colors.gray12,
-});
+const Heading = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 16px 0 0;
+  height: 45px;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+  color: ${theme.colors.gray12.computedValue};
+`;
