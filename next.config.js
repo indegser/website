7595641525e4d91dbb4f608ddd6f44f6fde4fe7e
indegser <indegser@gmail.com>
2023-05-05 15:39:48 +0900

    Remove sanity

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      'cdn.sanity.io',
       'i.ytimg.com',
       process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '') || '',
     ],
