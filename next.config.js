d8022ecc79cc5ed2aca7037dde3a59bee3c0fcdc
indegser <indegser@gmail.com>
2023-05-05 16:08:40 +0900

    Add rewrites

--- next.config.js
+++ next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '') || '',
     ],
   },
+  rewrites: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/index/82649fda5ba84801a464d7ef2f7552b3',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
