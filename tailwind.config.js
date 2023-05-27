180e277b57e81f1ab5e083e17a2c21ef562da146
indegser <indegser@gmail.com>
2023-05-27 22:27:38 +0900

    feat: Update nav tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    colors: {
+      gray: colors.gray,
+      blue: colors.blue,
+      backdrop: 'rgba(255, 255, 255, 0.85)',
+    },
+  },
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [],
 };
