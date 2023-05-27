be7dfb294e73c1ff8ee75a6b40782615a52aa389
indegser <indegser@gmail.com>
2023-05-27 19:16:17 +0900

    fix: Fix container typings

--- src/design/atoms/Container.tsx
+++ src/design/atoms/Container.tsx
@@ -1,9 +1,9 @@
-import { PropsWithChildren } from 'react';
+import { HTMLProps } from 'react';
 
-export const PageContainer = (props: PropsWithChildren) => {
+export const PageContainer = (props: HTMLProps<HTMLDivElement>) => {
   return <div className="mx-auto box-content max-w-5xl px-5" {...props} />;
 };
 
-export const PageContent = (props: PropsWithChildren) => {
+export const PageContent = (props: HTMLProps<HTMLDivElement>) => {
   return <div className="mx-auto max-w-2xl" {...props} />;
 };
