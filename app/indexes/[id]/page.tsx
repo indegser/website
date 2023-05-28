3397173c2c6a92bb980fc3ba296f8fda609cbf78
indegser <indegser@gmail.com>
2023-05-28 14:29:07 +0900

    feat: Add index metadata

--- app/indexes/[id]/page.tsx
+++ app/indexes/[id]/page.tsx
@@ -1,12 +1,28 @@
 import { Hydrate, dehydrate } from '@tanstack/react-query';
 import { get } from '@vercel/edge-config';
+import { Metadata } from 'next';
 
 import { IndexPage } from '@src/pages/index/IndexPage';
 import { getQueryClient } from '@src/queries/getQueryClient';
 import { createDatabaseQueryConfig } from '@src/queries/useDatabaseQuery';
 import { createIndexQueryConfig } from '@src/queries/useIndexQuery';
+import { notionUtils } from '@src/utils/notion';
 
 export const revalidate = 60;
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = params;
+  const queryClient = getQueryClient();
+  const result = await queryClient.fetchQuery(createIndexQueryConfig(id));
+
+  return {
+    title: notionUtils.getTitle(result),
+  };
+}
 
 export async function generateStaticParams() {
   const indexes = await get<{ id: string }[]>('indexes');
