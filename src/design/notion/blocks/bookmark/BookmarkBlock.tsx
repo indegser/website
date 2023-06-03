ca2f1e2d9d5b88dc21406f7b4b706c75a1045483
indegser <indegser@gmail.com>
2023-06-03 16:07:42 +0900

    feat: Turn bookmark block to server module

--- src/design/notion/blocks/bookmark/BookmarkBlock.tsx
+++ src/design/notion/blocks/bookmark/BookmarkBlock.tsx
@@ -1,9 +1,7 @@
-'use client';
-
 import { RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints';
-import { motion } from 'framer-motion';
+import { Suspense } from 'react';
 
-import { useBookmarkBlock } from './BookmarkBlock.hooks';
+import { BookmarkContent } from './BookmarkContent';
 import { Caption } from '../Caption';
 
 interface Props {
@@ -13,32 +11,18 @@ interface Props {
 
 export const BookmarkBlock = (props: Props) => {
   const { url, caption = [] } = props;
-  const { data: metadata } = useBookmarkBlock(url);
 
   return (
     <figure className="my-12">
-      <a href={url} title={metadata?.title} target="_blank" rel="noreferrer">
-        <motion.div whileTap={{ opacity: 0.8 }} transition={{ duration: 0.2 }}>
-          <div className="flex rounded-sm border">
-            <div className="grid basis-3/4 gap-1 p-4">
-              <div className="text-sm text-gray-900">{metadata?.title}</div>
-              <div className="line-clamp-2 text-xs text-gray-500">
-                {metadata?.description}
-              </div>
-              <div className="line-clamp-1 text-xs text-gray-700">
-                {decodeURIComponent(url)}
-              </div>
-            </div>
-            <div
-              className="basis-1/4 bg-cover bg-center"
-              style={{
-                backgroundImage:
-                  metadata?.image_url && `url(${metadata.image_url})`,
-              }}
-            />
-          </div>
-        </motion.div>
-      </a>
+      <Suspense
+        fallback={
+          /** @ts-expect-error Async Server Component */
+          <BookmarkContent url={url} isSkeleton />
+        }
+      >
+        {/* @ts-expect-error Async Server Component */}
+        <BookmarkContent url={url} />
+      </Suspense>
       <Caption caption={caption} />
     </figure>
   );
---
+++ src/design/notion/blocks/bookmark/BookmarkContent.tsx
@@ -0,0 +1,46 @@
+import { ORIGIN } from '@src/types/constants';
+
+interface OpenGraphType {
+  id: string;
+  url: string;
+  title: string;
+  description: string;
+  image_url: string;
+}
+
+interface Props {
+  url: string;
+  isSkeleton: boolean;
+}
+
+export const BookmarkContent = async ({ url, isSkeleton }: Props) => {
+  const { title, description, image_url } = isSkeleton
+    ? { title: '...', description: '...', image_url: '' }
+    : await fetch(`${ORIGIN}/api/og?url=${encodeURIComponent(url)}`).then(
+        (res) => res.json() as Promise<OpenGraphType>
+      );
+
+  return (
+    <a href={url} title={title} target="_blank" rel="noreferrer">
+      <div>
+        <div className="flex rounded-sm border">
+          <div className="grid basis-2/3 gap-1 p-4">
+            <div className="text-sm text-gray-900">{title}</div>
+            <div className="line-clamp-2 text-xs text-gray-500">
+              {description}
+            </div>
+            <div className="line-clamp-1 text-xs text-gray-700">
+              {decodeURIComponent(url)}
+            </div>
+          </div>
+          <div
+            className="basis-1/3 bg-gray-100 bg-cover bg-center"
+            style={{
+              backgroundImage: image_url && `url(${image_url})`,
+            }}
+          />
+        </div>
+      </div>
+    </a>
+  );
+};
