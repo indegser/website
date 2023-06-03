74c20b2840d1dab2218858bed5533b276b3479d1
indegser <indegser@gmail.com>
2023-06-03 23:59:41 +0900

    feat: Add ImageBlock

--- src/design/notion/blocks/Caption.tsx
+++ src/design/notion/blocks/Caption.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const Caption = ({ caption }: Props) => {
   return (
-    <div className="mt-4 text-xs font-medium text-gray-500">
+    <div className="mt-4 px-6 text-sm text-gray-500">
       <RichText data={caption} />
     </div>
   );
--- src/design/notion/blocks/ImageBlock.tsx
+++ src/design/notion/blocks/ImageBlock.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
 import { useCallback, useEffect, useState } from 'react';
 
 import { Caption } from './Caption';
 
-import { mq } from '@src/design/mediaQueries';
+import { PageContent } from '@src/design/atoms/Container';
 import { BlockType } from '@src/types/notion';
 
 interface Props {
@@ -52,60 +50,13 @@ export const ImageBlock = ({ block }: Props) => {
   const { caption } = block.image;
 
   return (
-    <Container>
-      <ImageLayout>
-        <ImageElement src={url} alt="" layout={width > height ? 'x' : 'y'} />
-      </ImageLayout>
-      <CaptionContainer>
+    <PageContent>
+      <div className="-mx-6 my-11">
+        <div className="flex justify-center">
+          <img src={url} className="block md:rounded-md" />
+        </div>
         <Caption caption={caption} />
-      </CaptionContainer>
-    </Container>
+      </div>
+    </PageContent>
   );
 };
-
-const Container = styled.div`
-  margin: 44px 0;
-  ${mq('sm')} {
-    margin-left: calc(-1 * max(22px, env(safe-area-inset-left)));
-    margin-right: calc(-1 * max(22px, env(safe-area-inset-right)));
-  }
-`;
-
-const ImageLayout = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const ImageElement = styled.img<{ layout: 'x' | 'y' }>`
-  display: block;
-  border-radius: 8px;
-
-  ${({ layout }) =>
-    layout === 'x' &&
-    css`
-      max-width: 100%;
-      height: auto;
-    `}
-
-  ${({ layout }) =>
-    layout === 'y' &&
-    css`
-      width: auto;
-      max-height: 80vh;
-    `}
-
-  ${mq('sm')} {
-    border-radius: 0px;
-    width: 100% !important;
-    height: auto !important;
-    max-width: auto !important;
-    max-height: auto !important;
-  }
-`;
-
-const CaptionContainer = styled.div`
-  ${mq('sm')} {
-    margin-left: max(22px, env(safe-area-inset-left));
-    margin-right: max(22px, env(safe-area-inset-right));
-  }
-`;
