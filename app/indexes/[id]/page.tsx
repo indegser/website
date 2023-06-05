1febcdf131de40a3d186f0326c0d2cf7bda66914
indegser <indegser@gmail.com>
2023-06-06 03:14:11 +0900

    feat: Use swrInfinite on IndexPage

--- app/indexes/[id]/page.tsx
+++ app/indexes/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { Hydrate, dehydrate } from '@tanstack/react-query';
 import { get } from '@vercel/edge-config';
 import { Metadata } from 'next';
 
-import { IndexPage } from '@src/pages/index/IndexPage';
+import { IndexPage, preloadIndex } from '@src/pages/index/IndexPage';
 import { getQueryClient } from '@src/queries/getQueryClient';
-import { createDatabaseQueryConfig } from '@src/queries/useDatabaseQuery';
 import { createIndexQueryConfig } from '@src/queries/useIndexQuery';
 import { notionUtils } from '@src/utils/notion';
 
@@ -35,17 +33,9 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const queryClient = getQueryClient();
   const { id } = params;
 
-  await Promise.all([
-    queryClient.fetchQuery(createIndexQueryConfig(id)),
-    queryClient.prefetchInfiniteQuery(createDatabaseQueryConfig(id)),
-  ]);
-
-  return (
-    <Hydrate state={dehydrate(queryClient)}>
-      <IndexPage id={id} />
-    </Hydrate>
-  );
+  preloadIndex(id);
+  /* @ts-expect-error Async Server Component */
+  return <IndexPage id={id} />;
 }
--- src/pages/index/IndexPage.tsx
+++ src/pages/index/IndexPage.tsx
@@ -1,44 +1,30 @@
-'use client';
-
-import { InView } from 'react-intersection-observer';
-
+import { IndexShowMore } from './IndexShowMore';
 import { RichItem } from './RichItem';
 
+import { databaseApi } from '@src/apis/database';
 import { PageContainer } from '@src/design/atoms/Container';
-import { Spinner } from '@src/design/atoms/Spinner';
-import { useDatabaseQuery } from '@src/queries/useDatabaseQuery';
-import { usePageTracking } from '@src/utils/analytics/usePageTracking';
+import { JournalPageType } from '@src/types/notion';
 
 interface Props {
   id: string;
 }
 
-export const IndexPage = ({ id }: Props) => {
-  usePageTracking('visit_index');
+export const preloadIndex = (id: string) => {
+  void databaseApi.queryDatabase({ id });
+};
 
-  const { data, isFetchingNextPage, fetchNextPage } = useDatabaseQuery(id);
+export const IndexPage = async ({ id }: Props) => {
+  const { results, next_cursor } =
+    await databaseApi.queryDatabase<JournalPageType>({ id });
 
   return (
     <PageContainer>
       <div className="grid grid-cols-1 gap-8 py-8 sm:grid-cols-3">
-        {data?.pages
-          .flatMap((page) => page.results)
-          .map((journal) => (
-            <RichItem key={journal.id} page={journal} />
-          ))}
+        {results.map((page) => (
+          <RichItem key={page.id} page={page} />
+        ))}
+        <IndexShowMore id={id} startCursor={next_cursor} />
       </div>
-      {isFetchingNextPage ? (
-        <Spinner />
-      ) : (
-        <InView
-          as="div"
-          style={{ height: 1 }}
-          onChange={(inView) => {
-            if (!inView) return;
-            fetchNextPage();
-          }}
-        />
-      )}
     </PageContainer>
   );
 };
