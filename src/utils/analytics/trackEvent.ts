125433eb278125a369805664af7191b06af06a24
indegser <indegser@gmail.com>
2023-06-06 13:04:55 +0900

    feat: Remove amplitude

--- src/utils/analytics/trackEvent.ts
+++
@@ -1,7 +0,0 @@
-import { amplitudeSdk } from '@src/sdks/amplitude';
-
-export const trackEvent = (event: string, data?: any) => {
-  if (!event) return;
-
-  amplitudeSdk.getInstance().logEvent(event, data);
-};
