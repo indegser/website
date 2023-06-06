473945c6a912ef3eeb8646041782a50d49d3eb08
indegser <indegser@gmail.com>
2023-06-06 12:28:01 +0900

    feat: Restore relation

--- src/pages/index/Relation.tsx
+++ src/pages/index/Relation.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import useSWR from 'swr';
+
 import { pageApi } from '@src/apis/content';
 import { IndexConfigType } from '@src/types/indexes';
 import { PageType } from '@src/types/notion';
@@ -8,15 +12,17 @@ interface Props {
   config: IndexConfigType;
 }
 
-export const Relation = async ({ page, config }: Props) => {
-  const ids = notionUtils.getRelationOfPage(page, config);
-  const results = await Promise.all(ids.map((id) => pageApi.getPage(id)));
+export const Relation = ({ page, config }: Props) => {
+  const { data } = useSWR(`relation-of-${page.id}`, () => {
+    const ids = notionUtils.getRelationOfPage(page, config);
+    return Promise.all(ids.map((id) => pageApi.getPage(id)));
+  });
 
-  if (results.length === 0) return null;
+  if (!data || data?.length === 0) return null;
 
   return (
     <div className="flex flex-wrap gap-1">
-      {results.map((page) => {
+      {data.map((page) => {
         return (
           <div
             className="rounded bg-blue-50 px-2 py-1 text-[11px] leading-relaxed"
