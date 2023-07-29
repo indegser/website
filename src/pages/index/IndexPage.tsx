e99d217f087f1973f127d8b440e0c5bc25056752
indegser <indegser@gmail.com>
2023-07-30 00:18:05 +0900

    feat: Update index page to use supabase

--- src/pages/index/IndexPage.tsx
+++ src/pages/index/IndexPage.tsx
@@ -1,10 +1,8 @@
-import { IndexShowMore } from './IndexShowMore';
 import { RichItem } from './RichItem';
 
-import { configApi } from '@src/apis/config';
 import { databaseApi } from '@src/apis/database';
 import { PageContainer } from '@src/design/atoms/Container';
-import { ContentType } from '@src/types/notion';
+import { supabase } from '@src/sdks/supabase';
 
 interface Props {
   id: string;
@@ -14,20 +12,15 @@ export const preloadIndex = (id: string) => {
   void databaseApi.queryDatabase({ id });
 };
 
-export const IndexPage = async ({ id }: Props) => {
-  const { results, next_cursor } = await databaseApi.queryDatabase<ContentType>(
-    { id }
-  );
-
-  const config = await configApi.getConfig(id);
+export const IndexPage = async (props: Props) => {
+  const { data } = await supabase.from('pages').select('*');
 
   return (
     <PageContainer>
       <div className="grid grid-cols-1 gap-8 py-8 sm:grid-cols-3">
-        {results.map((page) => (
-          <RichItem key={page.id} config={config} page={page} />
+        {data.map((page) => (
+          <RichItem key={page.id} page={page} />
         ))}
-        <IndexShowMore id={id} config={config} startCursor={next_cursor} />
       </div>
     </PageContainer>
   );
--- src/pages/index/RichItem.tsx
+++ src/pages/index/RichItem.tsx
@@ -1,34 +1,30 @@
 import Link from 'next/link';
 import Balancer from 'react-wrap-balancer';
 
-import { Relation } from './Relation';
 import { RichItemThumbnail } from './RichItemThumbnail';
 
 import { Time } from '@src/design/atoms/Time';
-import { IndexConfigType } from '@src/types/indexes';
-import { PageType, PropertyType } from '@src/types/notion';
-import { getNotionFileUrl, notionUtils } from '@src/utils/notion';
 
 interface Props {
-  page: PageType;
-  config: IndexConfigType;
+  page: {
+    id: string;
+    title: string;
+    cover: string;
+    last_edited_time: string;
+    created_time: string;
+  };
 }
 
-export const RichItem = ({ page, config }: Props) => {
-  const { id, last_edited_time } = page;
+export const RichItem = ({ page }: Props) => {
+  const { id, title, cover, last_edited_time } = page;
 
-  const title = notionUtils.getTitle(page);
-
-  const href = config?.urlProperty
-    ? (page.properties[config.urlProperty] as PropertyType<'url'>).url
-    : `/content/${id}`;
+  const href = `/content/${id}`;
 
   return (
     <Link href={href}>
       <section className="grid auto-rows-max gap-2">
-        <RichItemThumbnail src={getNotionFileUrl(page.cover)} alt={title} />
+        <RichItemThumbnail src={cover} alt={title} />
         <div className="grid gap-2">
-          <Relation page={page} config={config} />
           <div className="m-0 font-semibold leading-tight">
             <Balancer>{title}</Balancer>
           </div>
