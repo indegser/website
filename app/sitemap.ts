cf7e3109ec3f9be330cd545cec9040719f38e0d3
indegser <indegser@gmail.com>
2023-07-31 01:43:31 +0900

    feat: Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+import { pageApi } from '@src/apis/page.api';
+import { ORIGIN } from '@src/types/const.types';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { data } = await pageApi.queryPages({ limit: 100 });
+  return data.map((page) => ({
+    url: `${ORIGIN}/content/${page.id}`,
+    lastModified: page.last_edited_time,
+  }));
+}
