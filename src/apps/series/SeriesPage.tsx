d5fc3fb9149c4b381bb6897570241ff893d214f1
indegser <indegser@gmail.com>
2023-08-01 00:32:29 +0900

    feat: Add series name

--- src/apps/series/SeriesPage.tsx
+++ src/apps/series/SeriesPage.tsx
@@ -8,10 +8,14 @@ interface Props {
 }
 
 export const SeriesPage = async ({ id }: Props) => {
+  const { data: series } = await seriesApi.getSeries(id);
   const { data } = await seriesApi.getSeriesEpisodes(id);
 
   return (
     <PageContainer>
+      <div className="pt-8">
+        <h1 className="text-4xl font-extrabold">{series.name}</h1>
+      </div>
       <div className="grid grid-cols-1 gap-8 py-8 sm:grid-cols-3">
         {data?.map(({ series, ...page }) => (
           <RichItem key={page.id} page={page} series={series} />
