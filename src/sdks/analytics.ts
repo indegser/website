b624f98b3477b5be6d45c0a93144d11de28dff99
indegser <indegser@gmail.com>
2023-08-05 10:37:26 +0900

    feat: Add Feedback inView

--- src/sdks/analytics.ts
+++ src/sdks/analytics.ts
@@ -1,3 +1,9 @@
 import * as sdk from '@amplitude/analytics-browser';
 
-sdk.init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY);
+import { isProduction } from '@src/types/env.types';
+
+sdk.init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY, {
+  logLevel: isProduction ? sdk.Types.LogLevel.None : sdk.Types.LogLevel.Debug,
+});
+
+export const amplitude = sdk;
