daca63c7a504e5a1da74e82e005a85fa13890530
indegser <indegser@gmail.com>
2023-10-02 05:56:38 +0900

    feat: Add database server action

---
+++ app/database/page.tsx
@@ -0,0 +1,47 @@
+import {
+  createServerActionClient,
+  createServerComponentClient,
+} from '@supabase/auth-helpers-nextjs';
+
+import { Database } from 'lib/supabase/types';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+
+export default async function Page() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data } = await supabase.from('databases').select('*');
+  const { data: sessionData } = await supabase.auth.getSession();
+
+  if (!sessionData.session) {
+    return <div>Should authenticate first</div>;
+  }
+
+  const addDatabase = async (formData: FormData) => {
+    'use server';
+
+    const supabase = createServerActionClient<Database>({ cookies });
+    const id = String(formData.get('id'));
+
+    const { error } = await supabase
+      .from('databases')
+      .upsert({ id, user_id: sessionData.session.user.id });
+
+    if (error) {
+      console.error(error.message);
+      return;
+    }
+
+    revalidatePath('/database');
+  };
+
+  return (
+    <div style={{ color: 'white' }}>
+      <form action={addDatabase}>
+        <input type="text" name="id" style={{ color: 'black' }}></input>
+      </form>
+      <pre style={{ whiteSpace: 'pre-wrap' }}>
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
+  );
+}
