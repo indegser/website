a3f4450c015c4470372fafafadfee2a12b6dc7bd
indegser <indegser@gmail.com>
2023-10-14 14:33:43 +0900

    feat: Fix database page

--- app/database/[database_id]/page.tsx
+++ app/database/[database_id]/page.tsx
@@ -1,20 +1,28 @@
 import { PageContainer } from '@/components/atoms/Container';
 import { RichItem } from '@/components/organs/rich-item/RichItem';
+import { Database } from '@/lib/supabase';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { syncApi } from 'lib/utils/sync';
 import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
 
 const getData = async (databaseId: string) => {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { id, token },
-  } = await supabase
-    .from('databases')
-    .select()
-    .eq('id', databaseId)
-    .maybeSingle();
+  const supabase = createServerComponentClient<Database>({ cookies });
 
-  return syncApi.syncDatabase(id, token);
+  const { data } = await supabase
+    .from('tokens')
+    .select()
+    .single()
+    .throwOnError();
+
+  const { token } = data!;
+
+  try {
+    const result = await syncApi.syncDatabase(databaseId, token);
+    return result;
+  } catch (err) {
+    return { data: null, error: err };
+  }
 };
 
 export default async function Page({
@@ -22,7 +30,11 @@ export default async function Page({
 }: {
   params: { database_id: string };
 }) {
-  const { data } = await getData(params.database_id);
+  const { data, error } = await getData(params.database_id);
+
+  if (error) {
+    return notFound();
+  }
 
   return (
     <PageContainer>
