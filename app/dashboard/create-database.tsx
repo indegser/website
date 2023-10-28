0a2041497ebf8dcd27a56f90c3db4a95a90211b7
indegser <indegser@gmail.com>
2023-10-28 23:21:47 +0900

    feat: Add form status

--- app/dashboard/create-database.tsx
+++ app/dashboard/create-database.tsx
@@ -1,73 +1,33 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { createDatabase } from '@/lib/actions/create-database';
 import { PlusIcon } from '@radix-ui/react-icons';
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
-import { notion } from 'lib/notion';
 
-import { Database } from 'lib/supabase/types';
-import { revalidatePath } from 'next/cache';
-import { cookies } from 'next/headers';
-import { z } from 'zod';
+import { useFormState } from 'react-dom';
 
-const schema = z.object({
-  id: z.string(),
-});
-
-export default async function CreateDatabase() {
-  const addDatabase = async (formData: FormData) => {
-    'use server';
-
-    const supabase = createServerActionClient<Database>({ cookies });
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
-
-    if (!session) return;
-
-    const { data, error } = await supabase
-      .from('tokens')
-      .select('token')
-      .eq('user_id', session.user.id)
-      .single();
-
-    if (error) return;
-    const { token } = data;
-    const id = String(formData.get('id'));
-
-    try {
-      const database = await notion.databases.retrieve({
-        database_id: id,
-        auth: token,
-      });
-
-      const { error } = await supabase.from('databases').upsert({
-        id: database.id,
-        user_id: session.user.id,
-        raw_data: database,
-      });
-
-      if (error) {
-        console.error(error.message);
-        return;
-      }
-
-      revalidatePath('/dashboard');
-    } catch (err) {
-      console.warn(err);
-    }
-  };
+export default function CreateDatabase() {
+  const [state, formAction] = useFormState(createDatabase, {
+    message: undefined,
+  });
 
   return (
     <div className="mb-4">
-      <form action={addDatabase}>
-        <div className="flex space-x-4">
-          <Input type="text" name="id" />
+      <form action={formAction}>
+        <div className="flex items-center space-x-4">
+          <Input
+            type="text"
+            name="id"
+            placeholder="Database ID ex) 102e395151ae486680dcd33049fe281d"
+          />
           <div className="flex-shrink-0">
             <Button type="submit">
               <PlusIcon className="mr-2 h-4 w-4" /> Add New...
             </Button>
           </div>
         </div>
+        <div>{state?.message}</div>
       </form>
     </div>
   );
---
+++ lib/actions/create-database.ts
@@ -0,0 +1,54 @@
+'use server';
+
+import { isNotionClientError } from '@notionhq/client';
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import { notion } from '../notion';
+import { Database } from '../supabase';
+
+export async function createDatabase(
+  prevState: { message?: string } | undefined,
+  formData: FormData,
+) {
+  const supabase = createServerActionClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) return;
+
+  const { data, error } = await supabase
+    .from('tokens')
+    .select('token')
+    .eq('user_id', session.user.id)
+    .single();
+
+  if (error) return { message: error.message };
+
+  const { token } = data;
+  const id = String(formData.get('id'));
+
+  try {
+    const database = await notion.databases.retrieve({
+      database_id: id,
+      auth: token,
+    });
+
+    const { error } = await supabase.from('databases').upsert({
+      id: database.id,
+      user_id: session.user.id,
+      raw_data: database,
+    });
+
+    if (error) {
+      return { message: error.message };
+    }
+
+    revalidatePath('/dashboard');
+  } catch (err) {
+    if (isNotionClientError(err)) {
+      return { message: err.message };
+    }
+  }
+}
