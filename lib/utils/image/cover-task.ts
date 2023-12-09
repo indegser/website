5f702e8aabb79adf6ade50bd9864768602baf8e7
indegser <indegser@gmail.com>
2023-12-09 13:44:59 +0900

    feat: Remove supabase image upload

--- lib/utils/image/cover-task.ts
+++ lib/utils/image/cover-task.ts
@@ -1,28 +1,8 @@
 import { notionUtils } from '../notion';
-import { uploadImageToSupabase } from './upload';
 
-import { CDN_ORIGIN } from 'lib/constants';
 import { notion } from 'lib/notion';
 import { PageType } from 'lib/supabase/notion.types';
 import { uploadImage } from './create-image';
-
-export const coverTask = (pages: Array<PageType>) => {
-  return pages.map(async (page) => {
-    const coverUrl = notionUtils.getNotionFileUrl(page.cover);
-    const shouldReplace = coverUrl && !coverUrl.includes(CDN_ORIGIN);
-
-    if (!shouldReplace) return page;
-
-    const newCoverUrl = await uploadImageToSupabase(coverUrl, 'cover');
-
-    if (!newCoverUrl) return page;
-
-    return notion.pages.update({
-      page_id: page.id,
-      cover: { external: { url: newCoverUrl.publicURL } },
-    }) as Promise<PageType>;
-  });
-};
 
 export const coverTask2 = async (page: PageType, auth?: string) => {
   const coverUrl = notionUtils.getNotionFileUrl(page.cover);
