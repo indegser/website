a9fd64fdff8faec26364b45b28b444b7a66ee1b3
indegser <indegser@gmail.com>
2023-12-30 21:33:11 +0900

    feat: Add title style

--- components/layout/content/content-headline.tsx
+++ components/layout/content/content-headline.tsx
@@ -1,3 +1,4 @@
+import { PageContainer } from '@/components/atoms/Container';
 import { ContentBlur } from './content-blur';
 import { ContentCover } from './content-cover';
 
@@ -18,20 +19,22 @@ export const ContentHeadline = async (props: Props) => {
         <section className="relative mb-8 aspect-[2/3] overflow-hidden md:aspect-video">
           <ContentCover src={cover} alt={title} />
           <div className="relative flex h-full items-end">
-            <div className="relative h-1/3 w-full">
+            <div className="relative h-1/3 w-full md:h-1/4">
               <ContentBlur />
-              <div className="relative flex flex-col gap-4 px-5 pt-5">
-                <h1 className="my-0 break-keep text-2xl font-black leading-none">
+              <PageContainer>
+                <h1 className="my-0 mt-2 -skew-x-12 break-keep text-2xl font-black leading-none md:text-3xl">
                   <Balancer>
-                    <span className="bg-white px-2 py-1 text-gray-950 mix-blend-lighten">
+                    <span className="bg-white px-2 py-1 text-gray-950">
                       {title}
                     </span>
                   </Balancer>
                 </h1>
-                <div className="relative line-clamp-2 flex h-full items-end text-sm text-gray-100 text-opacity-90">
-                  {excerpt}
+                <div className="relative flex flex-col gap-4 pt-5">
+                  <div className="relative line-clamp-2 flex h-full items-end text-sm text-gray-100 text-opacity-90 md:text-base">
+                    {excerpt}
+                  </div>
                 </div>
-              </div>
+              </PageContainer>
             </div>
           </div>
         </section>
