109d91974e267f30b98e2e7f9c5e45f43ebd8147
indegser <indegser@gmail.com>
2024-01-19 02:27:35 +0900

    feat: Add timezone

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,10 +1,15 @@
 import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { redirect } from 'next/navigation';
 import { CalendarPage } from './CalendarPage';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export default function Calendar({ searchParams }: { searchParams: any }) {
   if (!searchParams.id) {
-    const now = dayjs();
+    const now = dayjs().tz('Asia/Seoul');
     const tomorrow = now.add(1, 'day');
     const id = tomorrow.format('YYYYMMDD');
     return redirect(`/calendar?id=${id}`);
