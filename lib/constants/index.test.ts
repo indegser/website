8a58d7ce4b3f15d224b48609c1a826b10f6ee575
Jaekwon Han <indegser@gmail.com>
2025-02-08 21:42:32 +0900

    feat: Update getURL function to handle production environment and improve tests

--- lib/constants/index.test.ts
+++ lib/constants/index.test.ts
@@ -12,30 +12,20 @@ describe('getURL', () => {
     process.env = OLD_ENV; // Restore old environment
   });
 
-  it('should return the site URL from NEXT_PUBLIC_SITE_URL', () => {
-    process.env.NEXT_PUBLIC_SITE_URL = 'https://example.com';
-    expect(getURL()).toBe('https://example.com');
+  it('should return the correct URL when env is production', () => {
+    process.env.VERCEL_ENV = 'production';
+    expect(getURL()).toBe('https://www.indegser.com');
   });
 
-  it('should return the Vercel URL from NEXT_PUBLIC_VERCEL_URL', () => {
-    process.env.NEXT_PUBLIC_SITE_URL = '';
+  it('should return the Vercel URL when env is preview', () => {
+    process.env.VERCEL_ENV = 'preview';
     process.env.NEXT_PUBLIC_VERCEL_URL = 'example.vercel.app';
     expect(getURL()).toBe('https://example.vercel.app');
   });
 
   it('should return localhost URL when no environment variables are set', () => {
-    process.env.NEXT_PUBLIC_SITE_URL = '';
+    process.env.VERCEL_ENV = 'development';
     process.env.NEXT_PUBLIC_VERCEL_URL = '';
     expect(getURL()).toBe('http://localhost:3000');
-  });
-
-  it('should remove trailing slash from the URL', () => {
-    process.env.NEXT_PUBLIC_SITE_URL = 'https://example.com/';
-    expect(getURL()).toBe('https://example.com');
-  });
-
-  it('should add https:// to the URL if not present', () => {
-    process.env.NEXT_PUBLIC_SITE_URL = 'example.com';
-    expect(getURL()).toBe('https://example.com');
   });
 });
