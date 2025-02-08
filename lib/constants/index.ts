cdecbda594132d269b5c6af77f128b1c1b6ff25e
Jaekwon Han <indegser@gmail.com>
2025-02-08 14:36:19 +0900

    fix: Update getURL function to return production URL for indegser.com

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -12,14 +12,13 @@ export const GIT_COMMIT_SHA =
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 export const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/';
-  // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://${url}`;
-  url = url.charAt(url.length - 1) === '/' ? url.slice(0, -1) : url;
-  return url;
+  if (isProduction) {
+    return 'https://www.indegser.com';
+  }
+
+  return (
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || 'http://localhost:3000'
+  );
 };
 
 export const INDEGSER_DATABASE_ID = '82649fda5ba84801a464d7ef2f7552b3';
