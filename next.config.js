ad7cdb0de1e6a5eba07d4e78a476a721caa2a589
Jaekwon Han <indegser@gmail.com>
2025-02-09 22:01:19 +0900

    feat: Add Sanity Studio environment variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,15 @@ const nextConfig = {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true,
   },
+  /**
+   * Env 이름을 Sanity와 Next 동일하게 쓰기 위해
+   * Vercel secret에서 스튜디오 변수를 관리하고 이를
+   * 클라이언트에서도 쓸 수 있도록 NEXT_PULBIC prefix 없이도 사용할 수 있도록 env를 추가 설정함.
+   */
   env: {
     VERCEL_ENV: process.env.VERCEL_ENV,
+    SANITY_STUDIO_DATASET: process.env.SANITY_STUDIO_DATASET,
+    SANITY_STUDIO_PROJECT_ID: process.env.SANITY_STUDIO_PROJECT_ID,
   },
   serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
   images: {
