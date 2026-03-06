ca1a0be226077ae486e89de1c13c3f94464e47eb
Jaekwon Han <indegser@gmail.com>
2026-03-07 05:46:38 +0900

    Remove threads route and switch lint script to ESLint CLI

--- app/threads/components/thread-category.tsx
+++
@@ -1,9 +0,0 @@
-import { Category } from '@/lib/sanity';
-
-interface Props {
-  category?: Category | null;
-}
-
-export function ThreadCategory({ category }: Props) {
-  return <div className="font-semibold">{category?.title || 'Untitled'}</div>;
-}
