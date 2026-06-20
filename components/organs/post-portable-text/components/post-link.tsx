ddf48cb1cb56c10655c71ff348b78d42840efce8
Jaekwon Han <indegser@gmail.com>
2026-06-20 12:37:34 +0900

    fix(posts): keep internal links in same tab

--- components/organs/post-portable-text/components/post-link.tsx
+++ components/organs/post-portable-text/components/post-link.tsx
@@ -1,12 +1,16 @@
 import { PortableTextMarkComponent } from 'next-sanity';
 
 export const PostLink: PortableTextMarkComponent = ({ value, children }) => {
-  const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined;
+  const isInternal =
+    value.href.startsWith('/') ||
+    value.href.startsWith('?') ||
+    value.href.startsWith('#');
+  const rel = !isInternal ? 'noreferrer noopener' : undefined;
   return (
     <a
       href={value.href}
       rel={rel}
-      target="_blank"
+      target={!isInternal ? '_blank' : undefined}
       className="cursor-pointer text-blue-800 no-underline hover:underline dark:text-blue-400"
     >
       {children}
